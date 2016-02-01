fb8a65f30c7efa71d1dfb3cf0426c9eb29b60af8
Marius Darila <marius.darila@gmail.com>
2016-02-01 19:30:24 +0200

    feat(geth): add basic test case

---
+++ electron-api/modules/geth/geth.spec.js
@@ -0,0 +1,41 @@
+/* eslint strict: 0 */
+'use strict';
+const expect = require('chai').expect;
+
+describe('gethConnector', function () {
+  it('should provide executable for geth', function () {
+
+  });
+
+  it('should start geth process', function () {
+
+  });
+
+  it('should connect to IPC', function () {
+
+  });
+
+  it('should send rpc to ipc', function () {
+
+  });
+
+  it('should read data from rpc calls', function () {
+
+  });
+
+  it('should unlock account', function () {
+
+  });
+
+  it('should get blockchain sync status', function () {
+
+  });
+
+  it('should stop geth process', function () {
+
+  });
+
+  it('should destroy socket connection to IPC', function () {
+
+  });
+});
