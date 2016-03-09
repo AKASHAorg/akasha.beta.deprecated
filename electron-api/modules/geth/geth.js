19d9bc8ce0a65d4c8fac712bb751db00218ae7b0
Marius Darila <marius.darila@gmail.com>
2016-03-09 11:41:37 +0200

    chore(geth): add new binaries for download

--- electron-api/modules/geth/geth.js
+++ electron-api/modules/geth/geth.js
@@ -3,12 +3,12 @@
 
 const Binary = require('bin-wrapper');
 const path   = require('path');
-const base   = 'https://github.com/AkashaProject/geth-testnet/releases/download/1.3.3/';
+const base   = 'https://github.com/AkashaProject/geth-testnet/releases/download/1.3.5/';
 
 const geth = new Binary()
-  .src(base + 'Geth-Linux64-20160208.tar.bz2', 'linux', 'x64')
-  .src(base + 'Geth-Win64-20160208.zip', 'win32', 'x64')
-  .src(base + 'Geth-Darwin64-20160208.zip', 'darwin')
+  .src(base + 'Geth-Linux64-20160309.tar.bz2', 'linux', 'x64')
+  .src(base + 'Geth-Win64-20160309.zip', 'win32', 'x64')
+  .src(base + 'Geth-Darwin64-20160309.zip', 'darwin')
   .dest(path.join(__dirname, 'binary'))
   .use(process.platform === 'win32' ? 'geth.exe' : 'geth');
 
