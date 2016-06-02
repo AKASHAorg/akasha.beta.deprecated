c990793bdd3f14ce859c3f2ed24d137c4dc667c4
Sever Abibula <severs@akasha.world>
2016-06-02 17:04:42 +0300

    more works
    Signed-off-by: Sever Abibula <severs@akasha.world>

--- app/components/ui/entry-editor/components/custom-renderer.js
+++ app/components/ui/entry-editor/components/custom-renderer.js
@@ -1,20 +1,22 @@
 // import QuoteCaptionComponent from './blocks/blockquotecaption';
 // import CaptionComponent from './blocks/caption';
 // import AtomicBlock from './blocks/atomic';
+import MediaComponent from './blocks/media-component';
 
 export default (contentBlock) => {
-    // const type = contentBlock.getType();
-    // switch (type) {
+    const type = contentBlock.getType();
+    switch (type) {
     // case 'block-quote-caption': return {
     //     component: QuoteCaptionComponent
     // };
     // case 'caption': return {
     //     component: CaptionComponent
     // };
-    // case 'atomic': return {
-    //     component: AtomicBlock,
-    //     editable: false
-    // };
-    // default: return null;
-    // }
+    case 'atomic':
+        return {
+            component: MediaComponent,
+            editable: false
+        };
+    default: return null;
+    }
 };
--- app/components/ui/entry-editor/index.js
+++ app/components/ui/entry-editor/index.js
@@ -1,69 +1,99 @@
 import React, { Component, PropTypes } from 'react';
-import { TextField, SvgIcon } from 'material-ui';
-import { Editor, EditorState, getDefaultKeyBinding, CompositeDecorator, RichUtils } from 'draft-js';
+import { IconButton } from 'material-ui';
+import {
+    Editor,
+    EditorState,
+    getDefaultKeyBinding,
+    CompositeDecorator,
+    convertToRaw,
+    AtomicBlockUtils,
+    RichUtils,
+    Entity } from 'draft-js';
+import { getResizedImages } from '../../../utils/imageUtils';
 import styles from './style.css';
 import AddCircle from 'material-ui/svg-icons/content/add-circle-outline';
+import PhotoCircle from 'material-ui/svg-icons/image/add-a-photo';
 import { handleStrategy } from './strategies';
 import { HandleComponent } from './components';
 
 import EditorToolbar from './components/editor-toolbar';
 import rendererFn from './components/custom-renderer';
 
+const remote = require('remote');
+const dialog = remote.require('electron').dialog;
+
 class EntryEditor extends Component {
-    constructor(props) {
+    constructor (props) {
         super(props);
         const compositeDecorator = new CompositeDecorator([
             {
                 strategy: handleStrategy,
                 component: HandleComponent
             }
-        ])
+        ]);
         this.state = {
-            editorState: EditorState.createEmpty(compositeDecorator)
+            editorState: EditorState.createEmpty(compositeDecorator),
+            showAddButton: false,
+            toolbarVisible: false
         };
     }
-    render () {
-        const { editorState } = this.state;
-        return (
-            <div className = "editor" style = {{position: 'relative', textAlign: 'left'}}>
-                {this.state.showAddButton &&
-                    <div style = {{position: 'absolute', top: ((this.state.rows - 1) * 24) + 4, left: -32}}>
-                        <SvgIcon style={{cursor: 'pointer'}} onClick = {this._handleAddAttach}>
-                            <AddCircle color="#DDD" hoverColor = '#444'/>
-                        </SvgIcon>
-                    </div>
-                }
-                <Editor
-                    ref="editor" 
-                    editorState = {editorState}
-                    blockRendererFn = {rendererFn}
-                    onChange = {this._handleEditorChange}
-                    handleReturn = {this._handleReturn}
-                    placeholder = "Type your text"
-                    handleBeforeInput = {this._handleBeforeInput}
-                />
-                <EditorToolbar
-                    ref = "toolbar"
-                    editorState = {editorState}
-                    toggleBlockType = {this._toggleBlockType}
-                    toggleInlineStyle = {this._toggleInlineStyle}
-                    setLink = {this._setLink}
-                    focus = {this._focus}
-                />
-            </div>
-        );
+    getContent = () => {
+        const rawData = convertToRaw(this.state.editorState.getCurrentContent());
+        return rawData;
     }
     _handleEditorChange = (editorState) => {
-        console.log('change');
         this.setState({
-            editorState
+            editorState,
+        }, () => {
+            if (this.props.onChange) {
+                return this.props.onChange(editorState.getCurrentContent().getPlainText());
+            }
         });
     }
-    _handleBeforeInput(str) {
-        console.log(str, 'beforeInput');
+    _handleAddClick = () => {
+        this.setState({
+            lastSelection: this.state.editorState.getSelection().getAnchorKey(),
+            showAddButton: !this.state.showAddButton,
+        });
+    }
+    _handleBeforeInput = (str) => {
+        this.setState({
+            showAddButton: false
+        });
+    }
+    _toggleBlockType = (blockType, src) => {
+        const entityKey = Entity.create(blockType, 'IMMUTABLE', src[0]);
+        this.setState({
+            showAddButton: false
+        });
+        this._handleEditorChange(
+            AtomicBlockUtils.insertAtomicBlock(this.state.editorState, entityKey, ' ')
+        );
+    }
+    _addImage = () => {
+        dialog.showOpenDialog({
+            title: 'Select Image',
+            properties: ['openFile'],
+            filters: [{ name: 'Images', extensions: ['jpg', 'png', 'gif'] }]
+        }, files => {
+            if (!files) {
+                return;
+            }
+            const outputFilePromises = getResizedImages(files, {
+                minWidth: 1280,
+                imageWidths: ['mlarge']
+            });
+            Promise.all(outputFilePromises).then(results => {
+                this._toggleBlockType('image', results[0]);
+            }).catch(err => {
+                this.setState({
+                    error: err
+                });
+            });
+        });
     }
     _handleReturn = (ev) => {
-        if (e.shiftKey) {
+        if (ev.shiftKey) {
             this.setState({
                 editorState: RichUtils.insertSoftNewline(this.state.editorState)
             });
@@ -71,6 +101,93 @@ class EntryEditor extends Component {
         }
         return false;
     }
+    _getAddButtonStyles = () => {
+        const { editorState } = this.state;
+        const startKey = this.state.startKey || editorState.getSelection().getStartKey();
+        const selectionContent = editorState.getCurrentContent().getBlockForKey(startKey).getText();
+
+        const addButtonStyles = {
+            position: 'absolute',
+            opacity: 0,
+            left: -200,
+            zIndex: 5,
+            transition: 'opacity 0.218s ease-in-out, top 0.1s ease-in-out'
+        };
+
+        if (this._canShowAddButton(selectionContent, editorState)) {
+            const node = document.querySelector(`span[data-offset-key="${startKey}-0-0"]`);
+            let anchorNode = window.getSelection().anchorNode;
+
+            addButtonStyles.opacity = 1;
+            if (node) {
+                addButtonStyles.top = node.getBoundingClientRect().top - 12 + window.scrollY;
+                addButtonStyles.left = node.getBoundingClientRect().left - 48;
+            } else if (anchorNode && anchorNode.getBoundingClientRect) {
+                addButtonStyles.top = anchorNode.getBoundingClientRect().top + 20 + window.scrollY;
+                addButtonStyles.left = anchorNode.getBoundingClientRect().left - 48;
+            } else if (anchorNode && anchorNode.parentElement) {
+                anchorNode = anchorNode.parentElement;
+                addButtonStyles.top = anchorNode.getBoundingClientRect().top + 20 + window.scrollY;
+                addButtonStyles.left = anchorNode.getBoundingClientRect().left - 48;
+            }
+        }
+        return addButtonStyles;
+    }
+    _canShowAddButton = (selectionContent, editorState) => this.state.showAddButton ||
+        (selectionContent.length === 0 && editorState.getSelection().getHasFocus())
+    _testFocus = () => {
+        this.setState({
+            showAddButton: false,
+        });
+    }
+    render () {
+        const { editorState } = this.state;
+        const addButtonStyles = this._getAddButtonStyles();
+
+        return (
+            <div className = "editor" style = {{ textAlign: 'left' }}>
+                <div style = {addButtonStyles}>
+                    <IconButton
+                      onMouseDown = {this._handleAddClick}
+                      style = {{
+                          transform: this.state.showAddButton ? 'rotate(135deg)' : 'rotate(-180deg)'
+                      }}
+                    >
+                        <AddCircle color={this.state.showAddButton ? 'rgb(3, 169, 244)' : '#DDD'} />
+                    </IconButton>
+                    {this.state.showAddButton &&
+                        <IconButton onClick = {this._addImage} >
+                            <PhotoCircle />
+                        </IconButton>
+                    }
+                </div>
+                <div>
+                    <Editor
+                      ref="editor"
+                      editorState = {editorState}
+                      blockRendererFn = {rendererFn}
+                      onClick = {this._handleEditorClick}
+                      onChange = {this._handleEditorChange}
+                      handleReturn = {this._handleReturn}
+                      placeholder = {this.state.showAddButton ? '' : 'Type your text'}
+                      handleBeforeInput = {this._handleBeforeInput}
+                    />
+                </div>
+                <EditorToolbar
+                  ref = "toolbar"
+                  editorState = {editorState}
+                  toggleVisibility = {this._handleToolbarVisibility}
+                  toggleBlockType = {this._toggleBlockType}
+                  toggleInlineStyle = {this._toggleInlineStyle}
+                  setLink = {this._setLink}
+                />
+            </div>
+        );
+    }
 }
+
+EntryEditor.propTypes = {
+    onChange: PropTypes.func
+};
 
 export default EntryEditor;
