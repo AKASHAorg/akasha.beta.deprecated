fa4866bf9b83828b4b76e673a1283a949ac1c51b
jgabios <jajalinux@gmail.com>
2016-06-02 18:27:00 +0300

    eslint fixes

--- electron-api/modules/ipc/geth.js
+++ electron-api/modules/ipc/geth.js
@@ -1,9 +1,8 @@
-'use strict';
 
-const {ipcMain} = require('electron');
+const { ipcMain } = require('electron');
 const GethConnector = require('../services/geth/index.js');
-const {STATICS} = require('../settings');
-const {EVENTS} = require('../settings');
+const { STATICS } = require('../settings');
+const { EVENTS } = require('../settings');
 
 const symbolEnforcer = Symbol();
 const symbol = Symbol();
@@ -13,7 +12,7 @@ const symbol = Symbol();
  * It provides the View layer with access to geth instance.
  * It also registers events for the View layer, in order for the View layer
  * to be notified of what happens on the blockchain.
- * 
+ *
  */
 class GethService {
     /*
@@ -22,13 +21,14 @@ class GethService {
      * @param {Symbol} enforcer
      * @returns {GethService}
      */
-    constructor (enforcer){
+    constructor (enforcer) {
         if (enforcer !== symbolEnforcer) {
-          throw new Error('Cannot construct singleton');
+            throw new Error('Cannot construct singleton');
         }
         this.serverEvent = EVENTS.server.geth;
         this.clientEvent = EVENTS.client.geth;
         this.BLOCK_UPDATE_INTERVAL = 1000;
+        this.STARTSYNC_MSG = 'Start synchronizing with the network';
     }
     /**
      * Makes sure it returns the same reference to a GethService instance
@@ -36,88 +36,77 @@ class GethService {
      * @returns {GethService}
      */
     static getInstance () {
-      if (!this[symbol]) {
-        this[symbol] = new GethService(symbolEnforcer);
-      }
-      return this[symbol];
+        if (!this[symbol]) {
+            this[symbol] = new GethService(symbolEnforcer);
+        }
+        return this[symbol];
     }
     /*
      * It sets up the listeners for this module.
-     * Events used are: 
+     * Events used are:
      * server:geth:startService used by the View layer to start the geth executable
-     * 
+     *
      * @param {BrowserWindow} mainWindow
      * @returns undefined
      */
-    setupListeners ( mainWindow ) {
-        ipcMain.on(this.serverEvent.startService, ( event, arg ) => {
-            this._startGethService( event , arg );
+    setupListeners (mainWindow) {
+        ipcMain.on(this.serverEvent.startService, (event, arg) => {
+            this._startGethService(event, arg);
         });
-        ipcMain.on(this.serverEvent.stopUpdates, ( event, arg ) => {
-            this._stopGethUpdates( event , arg );
-            });
-        ipcMain.on(this.serverEvent.getUpdates, ( event, arg ) => {
-            this._getGethUpdates( event, arg);
+        ipcMain.on(this.serverEvent.stopUpdates, (event, arg) => {
+            this._stopGethUpdates(event, arg);
         });
-        ipcMain.on(this.serverEvent.stopService, ( event, arg ) => {
-            this._stopGethService( event, arg);
+        ipcMain.on(this.serverEvent.getUpdates, (event, arg) => {
+            this._getGethUpdates(event, arg);
         });
-
+        ipcMain.on(this.serverEvent.stopService, (event, arg) => {
+            this._stopGethService(event, arg);
+        });
     }
-
-    _sendEvent ( event ) {
-        return ( name, successCode, data ) => {
+    _sendEvent (event) {
+        return (name, successCode, data) => {
             event.sender.send(name, {
-                "success": successCode,
-                "status": data
+                success: successCode,
+                status: data
             });
         };
     }
+    _startGethService (event, arg) {
+        this.getGethService().start().then((data) => {
+            this._sendEvent(event)(this.clientEvent.startService, true, data);
+            this._sendEvent(event)(this.clientEvent.startSyncing, true, this.STARTSYNC_MSG);
 
-    _startGethService ( event, arg ) {
-        this.getGethService().start().then(( data ) => {
-          this._sendEvent(event)(this.clientEvent.startService, true, data);
-          this._sendEvent(event)(this.clientEvent.startSyncing, true, "Start synchronizing with the network");
-
-          setTimeout( () => {
-            this._getGethUpdates ( event, arg );
-          }, STATICS.GETH_SETPROVIDER_TIMEOUT + 1000);
-
-      }).catch((data) => {
-          event.sender.send(this.clientEvent.startService, false, data);
-      });
+            setTimeout(() => {
+                this._getGethUpdates(event, arg);
+            }, STATICS.GETH_SETPROVIDER_TIMEOUT + 1000);
+        }).catch((data) => {
+            event.sender.send(this.clientEvent.startService, false, data);
+        });
     }
-
-    _stopGethService ( event, arg ) {
-      this.getGethService().stop();
+    _stopGethService (event, arg) {
+        this.getGethService().stop();
     }
-
-    _getGethUpdates ( event, arg ) {
-            this.updatesFlag = setInterval( () => this._getBlockUpdates(event), this.BLOCK_UPDATE_INTERVAL);
+    _getGethUpdates (event, arg) {
+        this.updatesFlag = setInterval(() => this._getBlockUpdates(event),
+            this.BLOCK_UPDATE_INTERVAL);
     }
-
-    _stopGethUpdates ( event, arg ) {
-            clearInterval ( this.updatesFlag );
+    _stopGethUpdates (event, arg) {
+        clearInterval(this.updatesFlag);
     }
-
-    getGethService (){
+    getGethService () {
         return GethConnector.getInstance();
     }
-
-    _getBlockUpdates (event){
+    _getBlockUpdates (event) {
         this.getGethService().inSync().then((data) => {
-            if(data.length > 0) {
+            if (data.length > 0) {
                 this._sendEvent(event)(this.clientEvent.syncUpdate, true, {
-                    "code": "ok",
-                    "status": {
-                        "currentBlock": data.length > 1 ? data[1].currentBlock: -1,
-                        "highestBlock": data.length > 1 ? data[1].highestBlock: -1,
-                        "startingBlock": data.length > 1 ? data[1].startingBlock: -1,
-                        "peerCount": data[0]
-                    }
+                    currentBlock: data.length > 1 ? data[1].currentBlock : -1,
+                    highestBlock: data.length > 1 ? data[1].highestBlock : -1,
+                    startingBlock: data.length > 1 ? data[1].startingBlock : -1,
+                    peerCount: data[0]
                 });
             } else {
-                this._sendEvent(event)(this.clientEvent.syncUpdate, true, "empty");
+                this._sendEvent(event)(this.clientEvent.syncUpdate, true, 'empty');
             }
         }).catch((err) => {
             this._stopGethUpdates();
