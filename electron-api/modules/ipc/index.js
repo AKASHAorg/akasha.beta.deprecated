076a6e800bb24d5de47207f2cdde89ef1421805b
jgabios <jajalinux@gmail.com>
2016-06-03 17:39:49 +0300

    ipfs service added + lintification

--- electron-api/modules/ipc/index.js
+++ electron-api/modules/ipc/index.js
@@ -1,13 +1,17 @@
-// const ipfs = require('./ipfs.js');
+const ipfs = require('./ipfs.js');
 const geth = require('./geth.js');
 const logger = require('./logger.js');
 
+// initialize the AkashaLogger with the user data folder
+const app = require('electron').app;
+require('../../loggers/').getInstance(app.getPath('userData'));
 
 export function initIPCServices (mainWindow) {
     const gethIPCService = geth.getInstance();
     const loggerIPCService = logger.getInstance();
+    const ipfsService = ipfs.getInstance();
 
-    // ipfs.setupListeners(mainWindow);
     gethIPCService.setupListeners(mainWindow);
     loggerIPCService.setupListeners(mainWindow);
+    ipfsService.setupListeners(mainWindow);
 }
