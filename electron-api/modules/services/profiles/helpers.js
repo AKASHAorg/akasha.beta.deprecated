13b148717918c14f3e6b0a2dcae06b1fb44d2e2d
Marius Darila <marius.darila@gmail.com>
2016-06-03 16:42:34 +0300

    chore(lint)

--- electron-api/modules/services/profiles/helpers.js
+++ electron-api/modules/services/profiles/helpers.js
@@ -5,41 +5,41 @@ const Promise = require('bluebird');
  * @returns {Promise.<T>|*|Function|any|!ManagedPromise.<*>}
  */
 export function getLocalProfiles () {
-  let profilesPool = [];
-  return gethInstance.web3.eth.getAccountsAsync().then((data) => {
-    data.forEach(
-      (account) => {
-        profilesPool.push(akasha.profileInstance.resolveProfile(account));
-      }
-    );
-    return Promise.all(profilesPool)
-      .map((profile, index) => {
-        if (profile.ipfs) {
-          return { userName: profile.name, ipfs: profile.ipfs, address: data[index] };
-        }
-        return false;
-      }).filter((onlyValid) => onlyValid);
-  }).catch((err) => err);
+    let profilesPool = [];
+    return gethInstance.web3.eth.getAccountsAsync().then((data) => {
+        data.forEach(
+            (account) => {
+                profilesPool.push(akasha.profileInstance.resolveProfile(account));
+            }
+        );
+        return Promise.all(profilesPool)
+            .map((profile, index) => {
+                if (profile.ipfs) {
+                    return { userName: profile.name, ipfs: profile.ipfs, address: data[index] };
+                }
+                return false;
+            }).filter((onlyValid) => onlyValid);
+    }).catch((err) => err);
 }
 
 export function login (address, password, timer) {
-  const requirements = [
-    akasha.profileInstance.existsProfileAddr(address),
-    gethInstance.web3.personal.unlockAccountAsync(address, password, timer)
-  ];
-  return Promise.all(requirements)
-    .then((data) => {
-      const isAccepted = data[0] && data[1];
-      if (isAccepted) {
-        gethInstance.web3.eth.defaultAccount = address;
-      }
-      return isAccepted;
-    }).catch(() => false);
+    const requirements = [
+        akasha.profileInstance.existsProfileAddr(address),
+        gethInstance.web3.personal.unlockAccountAsync(address, password, timer)
+    ];
+    return Promise.all(requirements)
+        .then((data) => {
+            const isAccepted = data[0] && data[1];
+            if (isAccepted) {
+                gethInstance.web3.eth.defaultAccount = address;
+            }
+            return isAccepted;
+        }).catch(() => false);
 }
 
 export function logout (address) {
-  return gethInstance.web3.personal.lockAccountAsync(address)
-    .then((done) => done);
+    return gethInstance.web3.personal.lockAccountAsync(address)
+        .then((done) => done);
 }
 
 
