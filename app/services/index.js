f1880d3424d9d7ca2d98c2da7f4ee3b2d50c78f2
Sever Abibula <severs@akasha.world>
2016-06-09 19:53:37 +0300

    refactoring in progress - adding classes
    Signed-off-by: Sever Abibula <severs@akasha.world>

---
+++ app/services/index.js
@@ -0,0 +1,5 @@
+export { HearthbeatService } from './hearthbeat-service';
+export { LoggingService } from './logging-service';
+export { ProfileService } from './profile-service';
+export { SettingsService } from './settings-service';
+export { SetupService } from './setup-service';
--- app/services/settings-service.js
+++ app/services/settings-service.js
@@ -2,16 +2,14 @@ import { ipcRenderer } from 'electron';
 import { EVENTS } from '../../electron-api/modules/settings';
 import settingsDB from './db/settings';
 
-export function saveSettings (table, settings) {
-    return new Promise((resolve, reject) => {
-        console.log(settings);
-        settingsDB.transaction('rw', settingsDB.geth, async() => {
-            resolve(await settingsDB[table].add({ settings }));
-        }).catch(err => {
-            console.log(err, 'db error');
-            return reject(err);
+class SettingsService {
+    saveSettings (table, settings) {
+        return new Promise((resolve, reject) => {
+            settingsDB.transaction('rw', settingsDB.geth, async() => {
+                resolve(await settingsDB[table].add({ settings }));
+            }).catch(err => reject(err));
         });
-    });
+    }
+    getSettings (table, query) {}
 }
-
-export function getSettings (table, query) {}
+export { SettingsService };
