e820cb846a73ddc2a2639e7931c8fe18a2cd6400
Marius Darila <marius.darila@gmail.com>
2016-06-09 13:08:48 +0300

    fix(geth): download endpoint

--- electron-api/modules/services/geth/geth.js
+++ electron-api/modules/services/geth/geth.js
@@ -7,8 +7,8 @@ const base = 'https://github.com/ethereum/go-ethereum/releases/download/v1.4.6/'
 
 const geth = new Binary()
     .src(base + 'geth-Linux64-20160606174600-1.4.6-0f036f6.tar.bz2', 'linux', 'x64')
-    .src(base + 'Geth-Win64-20160606174600-1.4.6-0f036f6.zip', 'win32', 'x64')
-    .src(base + 'geth-OSX-20160606174600-1.4.6-0f036f6.zip', 'darwin')
+    .src(base + 'Geth-Win64-20160606144531-1.4.6-0f036f6.zip', 'win32', 'x64')
+    .src(base + 'geth-OSX-20160606144052-1.4.6-0f036f6.zip', 'darwin')
     .dest(path.join(__dirname, 'bin'))
     .use(process.platform === 'win32' ? 'geth.exe' : 'geth');
 
