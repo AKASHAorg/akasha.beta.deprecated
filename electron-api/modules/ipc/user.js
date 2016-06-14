06b1bf580aeadf8c43176e38bbb840b1750ae512
jgabios <jajalinux@gmail.com>
2016-06-14 19:10:14 +0300

    user signup working code + username exists

--- electron-api/modules/ipc/user.js
+++ electron-api/modules/ipc/user.js
@@ -16,7 +16,7 @@ class UserService extends IpcService {
         super('user');
         this.IPFS_ADD_SIGNUP_FAIL = 'ipfs add user signup fail';
         this.NO_COINBASE_FAIL = 'no coinbase / no ethereum account';
-        this.password = null;
+        this.password = 'zz';
     }
     /*
      * It sets up the listeners for this module.
@@ -32,6 +32,9 @@ class UserService extends IpcService {
                 data: JSON.stringify(arg)
             });
         });
+        ipcMain.on(this.serverEvent.exists, (event, arg) => {
+            this._usernameExists(event, arg);
+        });
     }
 
     getPassword () {
@@ -42,6 +45,17 @@ class UserService extends IpcService {
         this.password = password;
     }
 
+    _usernameExists (event, arg) {
+        const geth = UserService.getService('geth').getGethService();
+        const web3 = geth.web3;
+        const registry = new dapples.class(web3).objects.registry;
+        registry.hasProfile.call(web3.fromUtf8(arg.username), {
+
+        }, function(err, res){
+            console.log(res);
+        });
+    }
+
     _signUp (event, arg) {
         UserService
             .getService('ipfs')
@@ -50,19 +64,23 @@ class UserService extends IpcService {
             .add(arg)
             .then((response) => {
                 const ipfsHash = response[0].Hash;
+                console.log('ipfs hash: '+ipfsHash);
                 const geth = UserService.getService('geth').getGethService();
                 const web3 = geth.web3;
                 web3.eth.getCoinbase((err, res) => {
+                    console.log('account: ' + res);
                     if (err) {
                         this._sendEvent(event)(this.clientEvent.signUp, false, this.NO_COINBASE_FAIL);
                     } else {
-                        web3.personal.unlockAccountAsync(res, this.password, 20000).then((data) => {
-                            const registry = new dapples.class(gethInstance.web3).objects.registry;
-                            const ipfsHashDelimiter = Math.floor(ipfsHash.length/2);
-                            registry.register(web3.fromUtf8(arg.username), [ipfsHash.substring(0,ipfsHashDelimiter), ipfsHash.substring(ipfsHashDelimiter)], {
-                                    gas: 800000
+                        web3.personal.unlockAccountAsync(res, this.password, 20000).then(() => {
+                            console.log('unlock went through');
+                            const registry = new dapples.class(web3).objects.registry;
+                            const ipfsHashDelimiter = Math.floor(ipfsHash.length / 2);
+                            registry.register(web3.fromUtf8(JSON.parse(arg.data).username), [ipfsHash.substring(0, ipfsHashDelimiter), ipfsHash.substring(ipfsHashDelimiter)], {
+                                    gas: 1900000
                                 }, (err, tx) => {
-                                geth.addFilter('tx', tx, (txInfo) => {
+                                console.log('transaction went through: ' + tx);
+                                UserService.getService('geth').addFilter('tx', tx, (txInfo) => {
                                     this._sendEvent(event)(this.clientEvent.signUp, true, txInfo);
                                 });
                             });
