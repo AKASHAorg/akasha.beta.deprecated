5877f8c1ec54fc4348437d357efadaed2da85c23
jgabios <jajalinux@gmail.com>
2016-06-22 18:49:50 +0300

    listAccounts and signup fixes

--- electron-api/modules/ipc/user.js
+++ electron-api/modules/ipc/user.js
@@ -18,7 +18,13 @@ class UserService extends IpcService {
         super('user');
         this.IPFS_ADD_SIGNUP_FAIL = 'ipfs add user signup fail';
         this.NO_COINBASE_FAIL = 'no coinbase / no ethereum account';
+        this.UNLOCK_COINBASE_FAIL = 'unlock account fail, check your password';
         this.password = 'zz';
+        this.textFields = ['username', 'firstName', 'lastName', 'description'];
+        this.CREATE_PROFILE_CONTRACT_GAS = 19000;
+        this.UNLOCK_INTERVAL = 2000;
+        this.FAUCET_URL = 'http://faucet.ma.cx:3000/donate/';
+        this.ZERO_ADDR = '0x0000000000000000000000000000000000000000';
     }
     /*
      * It sets up the listeners for this module.
@@ -28,11 +34,10 @@ class UserService extends IpcService {
      * @param {BrowserWindow} mainWindow -- ignored for now
      * @returns undefined
      */
-    setupListeners () {
+    setupListeners (mainWindow) {
+        this.bWin = mainWindow;
         ipcMain.on(this.serverEvent.signUp, (event, arg) => {
-            this._signUp(event, {
-                data: JSON.stringify(arg)
-            });
+            this._signUp(event, arg);
         });
         ipcMain.on(this.serverEvent.exists, (event, arg) => {
             this._usernameExists(event, arg);
@@ -60,14 +65,13 @@ class UserService extends IpcService {
     }
 
     _usernameExists (event, arg) {
-        const geth = UserService.getService('geth').getGethService();
-        const web3 = geth.web3;
+        const web3 = this.__getWeb3();
         const registry = new Dapple.class(web3).objects.registry;
-        registry.hasProfile.call(web3.fromUtf8(arg.username), {
+        registry.getById.call(web3.fromUtf8(arg.username), {
 
         }, (err, res) => {
             if (!err) {
-                this._sendEvent(event)(this.clientEvent.createCoinbase, true, res);
+                this._sendEvent(event)(this.clientEvent.exists, true, res);
             }
         });
     }
@@ -89,28 +93,61 @@ class UserService extends IpcService {
             const getByAddrPromise = Promise.promisify(registry.getByAddr.call);
             for (let i = 0; i < data.length; i++) {
                 const ethAccount = data[i];
-                profilePromises
-                .push(getByAddrPromise(ethAccount)
-                .then((profileContractAddress) => profileContractAddress));
+                profilePromises.push(
+                    getByAddrPromise(ethAccount)
+                    .then(
+                        (
+                            (eth) => (profileContractAddress) => {
+                                return {
+                                    eth,
+                                    profileContractAddress
+                                };
+                            }
+                        )(ethAccount)
+                    )
+                );
             }
 
             Promise.all(profilePromises).then((results) => {
                 for (let i = 0; i < results.length; i++) {
-                    const akashaProfileContractHash = results[i];
-                    if (akashaProfileContractHash !== '0x0000000000000000000000000000000000000000') {
-                        profile.at(akashaProfileContractHash).getIpfs.call((err, tuple) => {
-                            // console.log(web3.toUtf8(tuple[0]) + web3.toUtf8(tuple[1]));
-                            // TODO: get the ipfsHash and get user info
-                        });
+                    const akashaProfileContractHash = results[i].profileContractAddress;
+                    const ethAccount = results[i].ethAccount;
+                    if (akashaProfileContractHash !== this.ZERO_ADDR) {
+                        profile
+                            .at(akashaProfileContractHash)
+                            .getIpfs
+                            .call(((ethAddress) => {
+                                return (err, tuple) => {
+                                    const ipfsHash = web3.toUtf8(tuple[0]) + web3.toUtf8(tuple[1]);
+                                    this.
+                                        _getIpfsAPI()
+                                        .cat({
+                                            id: ipfsHash,
+                                            encoding: 'utf8'
+                                        })
+                                        .then((result) => {
+                                            this._sendEvent(event)(
+                                                this.clientEvent.listAccounts,
+                                                true,
+                                                Object.assign({ ethAddress, result }, data)
+                                            );
+                                        })
+                                        .catch((ipfsErr) => {
+                                            this._sendEvent(event)(
+                                                this.clientEvent.listAccounts,
+                                                false,
+                                                ipfsErr);
+                                        });
+                                };
+                            })(ethAccount));
                     }
                 }
             });
-            this._sendEvent(event)(this.clientEvent.listAccounts, true, data);
         });
     }
 
     _faucetEther (event, arg) {
-        const URL = 'http://faucet.ma.cx:3000/donate/' + arg.account; // eslint-disable-line prefer-template
+        const URL = this.FAUCET_URL + arg.account; // eslint-disable-line prefer-template
         request({
             uri: URL,
             method: 'GET',
@@ -127,34 +164,92 @@ class UserService extends IpcService {
         });
     }
 
+    _getIpfsAPI () {
+        return UserService
+                .getService('ipfs')
+                .getIpfsService()
+                .api;
+    }
+
+    _addToIpfs (data) {
+        return new Promise((resolve, reject) => {
+            this
+                ._getIpfsAPI()
+                .add(data)
+                .then((response) => resolve(response))
+                .catch((err) => reject(err));
+        });
+    }
+
+    _uploadImage (name, buffer) {
+        return new Promise((resolve, reject) => {
+            this._addToIpfs({
+                data: buffer,
+                options: {
+                    isPath: true
+                }
+            }).then((response) => resolve({ name,
+                    hash: response[0].Hash
+                })
+            ).catch((err) => reject(err));
+        });
+    }
+
+    _uploadImages (signupJSON) {
+        const imagePromises = [];
+        if (signupJSON.avatar) {
+            imagePromises.push(this._uploadImage('avatar', signupJSON.avatar));
+        }
+        if (signupJSON.bg1) {
+            imagePromises.push(this._uploadImage('bg1', signupJSON.bg1));
+        }
+        if (signupJSON.bg2) {
+            imagePromises.push(this._uploadImage('bg2', signupJSON.bg2));
+        }
+        if (signupJSON.bg3) {
+            imagePromises.push(this._uploadImage('bg3', signupJSON.bg3));
+        }
+        if (signupJSON.bg4) {
+            imagePromises.push(this._uploadImage('bg4', signupJSON.bg4));
+        }
+        return Promise.all(imagePromises).then((data) => {
+            const imageHashes = {};
+            for (const result of data) {
+                imageHashes[result.name] = result.hash;
+            }
+            return imageHashes;
+        }).catch((err) => err);
+    }
+
     _signUp (event, arg) {
-        UserService
-            .getService('ipfs')
-            .getIpfsService()
-            .api
-            .add(arg)
+        this
+        ._uploadImages(arg)
+        .then((imageHashes) => {
+            const fullProfileJSON = Object.assign({}, imageHashes);
+            for (const key of this.textFields) {
+                fullProfileJSON[key] = arg[key];
+            }
+            return this._addToIpfs({
+                data: JSON.stringify(fullProfileJSON)
+            })
             .then((response) => {
                 const ipfsHash = response[0].Hash;
                 const web3 = this.__getWeb3();
-                web3.eth.getCoinbase((err, res) => {
-                    if (err) {
-                        this.
-                            _sendEvent(event)(
-                                    this.clientEvent.signUp,
-                                    false,
-                                    this.NO_COINBASE_FAIL);
-                    } else {
-                        web3.personal.unlockAccountAsync(res, this.password, 20000).then(() => {
+                web3.eth.getCoinbaseAsync().then((res) => {
+                    web3
+                        .personal
+                        .unlockAccountAsync(res, this.password, this.UNLOCK_INTERVAL)
+                        .then(() => {
                             const registry = new Dapple.class(web3).objects.registry;
                             const ipfsHashDelimiter = Math.floor(ipfsHash.length / 2);
                             registry.register(
-                                web3.fromUtf8(JSON.parse(arg.data).username),
+                                web3.fromUtf8(arg.username),
                                 [
                                     ipfsHash.substring(0, ipfsHashDelimiter),
                                     ipfsHash.substring(ipfsHashDelimiter)
                                 ],
                                 {
-                                    gas: 1900000
+                                    gas: this.CREATE_PROFILE_CONTRACT_GAS
                                 }, (error, tx) => {
                                     if (!error) {
                                         UserService
@@ -167,20 +262,27 @@ class UserService extends IpcService {
                                             });
                                     }
                                 });
+                        }).catch((err) => {
+                            this._sendEvent(event)(this.clientEvent.signUp,
+                                                false,
+                                                this.UNLOCK_COINBASE_FAIL);
                         });
-                    }
+                }).catch((err) => {
+                    this._sendEvent(event)(this.clientEvent.signUp, false, this.NO_COINBASE_FAIL);
                 });
             })
-            .catch(() => {
+            .catch((err) => {
+                this._sendEvent(event)(this.clientEvent.signUp, false, this.IPFS_ADD_SIGNUP_FAIL);
+            });
+        })
+            .catch((err) => {
                 this._sendEvent(event)(this.clientEvent.signUp, false, this.IPFS_ADD_SIGNUP_FAIL);
             });
     }
 
     _registerProfile (event, arg) {
-        UserService
-            .getService('ipfs')
-            .getIpfsService()
-            .api
+        this
+            ._getIpfsAPI()
             .add(arg)
             .then((response) => {
                 const ipfsHash = response[0].Hash;
@@ -215,9 +317,13 @@ class UserService extends IpcService {
             });
     }
 
+    __getGeth () {
+        return UserService.getService('geth');
+    }
+
     __getWeb3 () {
-        return UserService
-                .getService('geth')
+        return this
+                .__getGeth()
                 .getGethService()
                 .web3;
     }
