0ded927efbd00368d7444cdb4869191309e4aa30
Sever Abibula <severs@akasha.world>
2016-06-23 19:06:06 +0300

    faucet ready

--- app/reducers/profileState.js
+++ app/reducers/profileState.js
@@ -4,10 +4,26 @@ import { fromJS, List, Map } from 'immutable';
 const initialState = fromJS({
     profiles: List(),
     loggedProfile: Map(),
-    newProfile: Map(),
-    messages: ''
+    tempProfile: Map({
+        firstName: '',
+        lastName: '',
+        userName: '',
+        password: '',
+        password2: '',
+        address: '',
+        optionalData: {
+            avatarFile: {},
+            backgroundImage: [],
+            about: '',
+            links: []
+        },
+        currentStatus: {
+            currentStep: '',
+            status: '',
+            message: ''
+        }
+    })
 });
-
 
 export default function profileState (state = initialState, action) {
     switch (action.type) {
@@ -16,13 +32,36 @@ export default function profileState (state = initialState, action) {
         case types.GET_PROFILES_LIST_ERROR:
             return state.merge({ profiles: [], messages: action.message });
         case types.GET_TEMP_PROFILE_SUCCESS:
-            return state.merge({ newProfile: action.profile[0] });
+            return state.update('tempProfile', () => Map(action.profile[0]));
+        case types.CREATE_ETH_ADDRESS_START:
+            return state.updateIn(['tempProfile', 'currentStatus'],
+                (cStatus) => Object.assign(cStatus, { status: 'pending' })
+            );
         case types.CREATE_ETH_ADDRESS_SUCCESS:
-            return state.setIn(['newProfile', 'profileData', 'address'], () => action.data.status);
+            return state.mergeDeep(fromJS({
+                tempProfile: {
+                    address: action.data.status,
+                    currentStatus: {
+                        status: 'finished'
+                    }
+                }
+            }));
+        case types.CREATE_ETH_ADDRESS_ERROR:
+            return state.updateIn(['tempProfile', 'currentStatus'],
+                (cStatus) => Object.assign(cStatus, { status: 'failed' })
+            );
         case types.FUND_FROM_FAUCET_START:
-            return state.setIn(['newProfile', 'stepStatus'], () => 'pending');
+            return state.updateIn(['tempProfile', 'currentStatus'],
+                (cStatus) => Object.assign(cStatus, { status: 'pending' })
+            );
         case types.FUND_FROM_FAUCET_SUCCESS:
-            return state.setIn(['newProfile', '']);
+            return state.updateIn(['tempProfile', 'currentStatus'],
+                (cStatus) => Object.assign(cStatus, { status: 'finished' })
+            );
+        case types.FUND_FROM_FAUCET_ERROR:
+            return state.updateIn(['tempProfile', 'currentStatus'],
+                (cStatus) => Object.assign(cStatus, { status: 'failed' })
+            );
         case types.AUTH_LOGIN:
             return state.merge({
                 authAddress: 'stub'
