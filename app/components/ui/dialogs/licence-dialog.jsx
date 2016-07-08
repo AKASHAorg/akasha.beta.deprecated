4e1ea8974e989f4df851bead308ae35eacd85340
Sever Abibula <severs@akasha.world>
2016-07-08 21:41:28 +0300

    licensing component

--- app/components/ui/dialogs/licence-dialog.jsx
+++ app/components/ui/dialogs/licence-dialog.jsx
@@ -4,14 +4,14 @@ import {
     RaisedButton,
     RadioButton,
     Checkbox,
-    Divider } from 'material-ui';
-import licences from './licences';
+    Divider,
+    FontIcon } from 'material-ui';
 
 class LicenceDialog extends React.Component {
     constructor (props) {
         super(props);
         this.state = {
-            mainLicence: props.defaultLicence || licences.find(lic => lic.id === '1')
+            mainLicence: props.defaultLicence || props.licences.find(lic => lic.id === '1')
         };
     }
     _handleDialogCancel = (ev) => {
@@ -19,33 +19,40 @@ class LicenceDialog extends React.Component {
     }
     _handleSubLicenceCheck = (ev, val) => {
         this.setState({
-            subLicence: licences.find(lic => lic.id === val)
+            subLicence: this.props.licences.find(lic => lic.id === val)
         });
     }
     _handleMainLicenceCheck = (ev, val) => {
         this.setState({
-            mainLicence: licences.find(lic => lic.id === val),
+            mainLicence: this.props.licences.find(lic => lic.id === val),
             subLicence: null
+        });
+    }
+    _handleDefaultLicenceSet = (ev, isChecked) => {
+        this.setState({
+            isDefault: isChecked
         });
     }
     render () {
         const selectedMainLicence = this.state.mainLicence;
         const selectedMainLicenceId = selectedMainLicence.id;
 
-        const radios = licences.filter(lic => lic.parent === null).map(licence => {
-            const sublicence = licences.filter(lic => lic.parent === licence.id).map(sublic => {
-                return (
-                  <RadioButton
-                    key={sublic.id}
-                    value={sublic.id}
-                    label={sublic.label}
-                    style={{ marginTop: 16, marginLeft: 34 }}
-                    onCheck={(ev, val) => this._handleSubLicenceCheck(ev, val)}
-                    checked={(this.state.subLicence && this.state.subLicence.id === sublic.id)}
-                  />
-                );
-            });
-            // console.log(this.state.selectedLicence, 'licence');
+        const radios = this.props.licences.filter(lic => lic.parent === null).map(licence => {
+            const sublicence = this.props.licences
+                                .filter(lic => lic.parent === licence.id)
+                                .map(sublic =>
+                                  <RadioButton
+                                    key={sublic.id}
+                                    value={sublic.id}
+                                    label={sublic.label}
+                                    style={{ marginTop: 16, paddingLeft: 34 }}
+                                    onCheck={(ev, val) => this._handleSubLicenceCheck(ev, val)}
+                                    checked={
+                                      (this.state.subLicence &&
+                                        this.state.subLicence.id === sublic.id)
+                                    }
+                                  />
+                                );
             return (
               <span key={licence.id}>
                 <RadioButton
@@ -57,31 +64,55 @@ class LicenceDialog extends React.Component {
                   }
                   onCheck={(ev, val) => this._handleMainLicenceCheck(ev, val)}
                 />
-                {(selectedMainLicenceId === licence.id || 
-                  (this.state.subLicence && this.state.subLicence.parent === licence.id)) && 
+                {(selectedMainLicenceId === licence.id ||
+                  (this.state.subLicence && this.state.subLicence.parent === licence.id)) &&
                   sublicence
                 }
               </span>
             );
         });
-
         return (
           <Dialog
             title="Licensing"
             open={this.props.isOpen}
             modal
             onRequestClose={this.props.onRequestClose}
+            contentStyle={{ maxWidth: '550px' }}
+            autoScrollBodyContent
           >
             {radios}
             <Divider style={{ marginTop: 16 }} />
-            <small>{'Description of the selected licence'}</small>
+            <div style={{ minHeight: 48 }}>
+              {this.state.subLicence &&
+                this.state.subLicence.description.map((licDescription, key) =>
+                  <small key={key} className="row middle-xs" style={{ marginTop: 8 }}>
+                    <FontIcon className="material-icons" style={{ fontSize: 18 }}>
+                      {licDescription.icon}
+                    </FontIcon>
+                    <div className="col-xs-11">
+                      {licDescription.text}
+                    </div>
+                  </small>
+                )
+              }
+            </div>
             <div className="row middle-xs" style={{ marginTop: 24 }}>
               <div className="col-xs-6">
-                <Checkbox label="Set as default licence" />
+                <Checkbox label="Set as default licence" onCheck={this._handleDefaultLicenceSet} />
               </div>
               <div className="col-xs-6 end-xs">
                 <RaisedButton label="cancel" onTouchTap={this._handleDialogCancel} />
-                <RaisedButton label="done" primary style={{ marginLeft: 8 }} />
+                <RaisedButton
+                  label="done"
+                  primary
+                  style={{ marginLeft: 8 }}
+                  onTouchTap={(ev) =>
+                      this.props.onDone(ev, {
+                          mainLicence: this.state.mainLicence,
+                          subLicence: this.state.subLicence,
+                          isDefault: this.state.isDefault
+                      })}
+                />
               </div>
             </div>
           </Dialog>
@@ -92,7 +123,9 @@ class LicenceDialog extends React.Component {
 LicenceDialog.propTypes = {
     isOpen: React.PropTypes.bool,
     onRequestClose: React.PropTypes.func,
-    defaultLicence: React.PropTypes.string
+    defaultLicence: React.PropTypes.string,
+    licences: React.PropTypes.array,
+    onDone: React.PropTypes.func
 };
 
 export default LicenceDialog;
