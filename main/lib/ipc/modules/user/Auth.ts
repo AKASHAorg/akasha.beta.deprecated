320031831fdd8244e399557dd6c55027548fcaf0
Marius Darila <marius.darila@gmail.com>
2016-08-25 20:09:34 +0300

    feat(auth): add session token validation

--- main/lib/ipc/modules/user/Auth.ts
+++ main/lib/ipc/modules/user/Auth.ts
@@ -1,18 +1,14 @@
 import { createCipher, createDecipher, randomBytes, Decipher, Cipher } from 'crypto';
 import { GethConnector, gethHelper } from '@akashaproject/geth-connector';
-import { fromRpcSig, ecrecover, isValidAddress } from 'ethereumjs-util';
-import { Logger } from 'winston';
+import { fromRpcSig, ecrecover, toBuffer, bufferToHex, pubToAddress } from 'ethereumjs-util';
+import * as Promise from 'bluebird';
 
+const randomBytesAsync = Promise.promisify(randomBytes);
 export default class Auth {
     private _encrypted: Buffer;
     private _decipher: Decipher;
     private _cipher: Cipher;
-    private _session: {signedToken: string, address: string, expiration: Date, vrs: {v: string, r: string, s: string}};
-    private _token: string;
-
-    constructor() {
-        this._generateRandom();
-    }
+    private _session: {address: string, expiration: Date, vrs: {v: string, r: string, s: string}};
 
     /**
      *
@@ -20,31 +16,29 @@ export default class Auth {
      * @returns {Bluebird<string>|PromiseLike<string>|Thenable<string>|Promise<string>}
      */
     generateKey(pass: any) {
-        this._encrypt(pass);
-        let transformed = GethConnector.getInstance()
-            .web3
-            .fromUtf8(this._read().toString('utf8'));
-        return GethConnector.getInstance()
-            .web3
-            .personal
-            .newAccountAsync(transformed)
+        return this._encrypt(pass)
+            .then(() => {
+                return GethConnector.getInstance()
+                    .web3
+                    .personal
+                    .newAccountAsync(this._read().toString('utf8'));
+            })
             .then((address: string) => {
-                transformed = null;
+                this.login(address, pass, 2);
                 return address;
             });
     }
 
     /**
      *
+     * @returns {Promise<TResult>|Bluebird<U>|PromiseLike<TResult>|Thenable<U>}
      * @private
      */
     private _generateRandom() {
-        randomBytes(256, (err, buff) => {
-            if (err) {
-                throw err;
-            }
-            this._cipher = createCipher('aes-256-cbc', buff.toString('hex'));
-            this._decipher = createDecipher('aes-256-cbc', buff.toString('hex'));
+        return randomBytesAsync(256).then((buff: Buffer) => {
+            this._cipher = createCipher('aes-256-ctr', buff.toString('hex'));
+            this._decipher = createDecipher('aes-256-ctr', buff.toString('hex'));
+            return true;
         });
     }
 
@@ -56,8 +50,10 @@ export default class Auth {
      */
     private _encrypt(key: any) {
         const keyTr = Buffer.from(key);
-        this._encrypted = Buffer.concat([this._cipher.update(keyTr), this._cipher.final()]);
-        return this;
+        return this._generateRandom().then(() => {
+            this._encrypted = Buffer.concat([this._cipher.update(keyTr), this._cipher.final()]);
+            return true;
+        });
     }
 
     /**
@@ -73,62 +69,67 @@ export default class Auth {
      *
      * @param acc
      * @param pass
-     * @param timer                                  number of minutes
+     * @param timer
      * @returns {Bluebird<U>}
      */
     protected login(acc: string, pass: Uint8Array[], timer?: number) {
-        if (!isValidAddress(acc)) {
-            throw new Error(`${acc} is an invalid address`);
-        }
+
         return gethHelper
             .hasKey(acc)
             .then((found) => {
                 if (!found) {
                     throw new Error(`local key for ${acc} not found`);
                 }
-                this._encrypt(pass);
                 // temporary until personal_sign is shipped
                 // follow here https://github.com/ethereum/go-ethereum/pull/2940
                 // @TODO: migrate to personal_sign when available
-                GethConnector.getInstance()
-                    .web3
-                    .personal
-                    .unlockAccountAsync(acc, this._read().toString('utf8'), 500)
+                this._encrypt(pass)
+                    .then(() => {
+                        return GethConnector.getInstance()
+                            .web3
+                            .personal
+                            .unlockAccountAsync(acc, this._read().toString('utf8'), 2000);
+                    })
                     .then((unlocked: boolean) => {
                         if (!unlocked) {
                             throw new Error(`invalid password`);
                         }
-                        randomBytes(256, (err, buff) => {
-                            if (err) {
-                                throw err;
-                            }
-                            this._token = buff.toString('hex');
-                            this._signSession(this._token)
+                        return randomBytesAsync(256);
+                    })
+                    .then((buff: Buffer) => {
+                            const token = GethConnector.getInstance()
+                                .web3
+                                .sha3(buff.toString('hex'), {encoding: 'hex'});
+                            return this._signSession(acc, token)
                                 .then((signedString: string) => {
                                     const expiration = new Date();
                                     expiration.setMinutes(expiration.getMinutes() + timer);
                                     this._session = {
-                                        signedToken: signedString,
                                         expiration,
                                         address: acc,
                                         vrs: fromRpcSig(signedString)
                                     };
                                     setTimeout(() => this._flushSession(), 1000 * 60 * timer);
+                                    return {token, expiration};
                                 });
-                        });
                     })
-                    .catch((err: any) => Logger)
+                    .catch((err: Error) => {
+                        return {error: err.message};
+                    })
                     .finally(() => GethConnector.getInstance().web3.personal.lockAccountAsync(acc));
             });
     }
 
     /**
      *
+     * @param token
      * @returns {boolean}
      */
-    public isLogged() {
+    public isLogged(token: any) {
+        let pubKey: string;
+        let ethAddr: Buffer;
         const now = new Date();
-        if (!this._session) {
+        if (!this._session || !token) {
             return false;
         }
 
@@ -136,7 +137,14 @@ export default class Auth {
             return false;
         }
         const {v, r, s} = this._session.vrs;
-        return ecrecover(this._token, v, r, s) === this._session.address;
+        try {
+            pubKey = bufferToHex(ecrecover(toBuffer(token), v, r, s));
+            ethAddr = pubToAddress(pubKey);
+            return bufferToHex(ethAddr) === this._session.address;
+        }catch (err) {
+            return false;
+        }
+
     }
 
     /**
@@ -145,25 +153,31 @@ export default class Auth {
      */
     private _flushSession() {
         this._session = null;
-        this._token = null;
         this._encrypted = null;
     }
 
-    // temporary
-    private _signSession(hash: string) {
+    /**
+     *
+     * @param account
+     * @param hash
+     * @returns {any}
+     * @private
+     */
+    private _signSession(account: string, hash: string) {
         return GethConnector.getInstance()
             .web3
             .eth
-            .signAsync(hash);
+            .signAsync(account, hash);
     }
 
     /**
      *
      * @param data
-     * @returns {Promise}
+     * @param token
+     * @returns {any}
      */
-    public signData(data: {}) {
-        if (this.isLogged()) {
+    public signData(data: {}, token: string) {
+        if (this.isLogged(token)) {
             return GethConnector.getInstance()
                 .web3
                 .personal
