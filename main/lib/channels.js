f638df7cf5c574ca17648784794b04f5a95d9699
Marius Darila <marius.darila@gmail.com>
2016-09-29 14:52:08 +0300

    fix(ipc): add api preloader for disabled nodeIntegration

--- main/lib/channels.js
+++ main/lib/channels.js
@@ -9,18 +9,18 @@ const hashPath = (...path) => {
     return hash.digest('hex');
 };
 const channels = {
-    auth: ['manager', 'login', 'logout', 'requestEther', 'generateEthKey', 'getLocalIdentities'],
-    tags: ['manager', 'create', 'exists', 'getTagId', 'getTagAt', 'isSubscribed', 'subscribe',
+    auth: ['login', 'logout', 'requestEther', 'generateEthKey', 'getLocalIdentities'],
+    tags: ['create', 'exists', 'getTagId', 'getTagAt', 'isSubscribed', 'subscribe',
         'unsubscribe', 'getSubPosition'],
-    entry: ['manager', 'publish', 'update', 'upvote', 'downvote', 'isOpenedToVotes', 'getVoteOf',
+    entry: ['publish', 'update', 'upvote', 'downvote', 'isOpenedToVotes', 'getVoteOf',
         'getVoteEndDate', 'getScore', 'getEntriesCount', 'getEntryOf', 'getEntry'],
-    comments: ['manager', 'publish', 'update', 'upvote', 'downvote', 'getScore'],
-    geth: ['manager', 'options', 'startService', 'stopService', 'restartService', 'syncStatus', 'logs', 'status'],
-    ipfs: ['manager', 'startService', 'stopService', 'status', 'resolve', 'getConfig', 'setPorts', 'getPorts'],
-    profile: ['manager', 'getProfileData', 'getMyBalance', 'getIpfs', 'unregister', 'follow', 'getFollowersCount',
+    comments: ['publish', 'update', 'upvote', 'downvote', 'getScore'],
+    geth: ['options', 'startService', 'stopService', 'restartService', 'syncStatus', 'logs', 'status'],
+    ipfs: ['startService', 'stopService', 'status', 'resolve', 'getConfig', 'setPorts', 'getPorts'],
+    profile: ['getProfileData', 'getMyBalance', 'getIpfs', 'unregister', 'follow', 'getFollowersCount',
         'getFollowingCount', 'getFollowers', 'getFollowing'],
-    registry: ['manager', 'profileExists', 'registerProfile', 'getCurrentProfile', 'getByAddress'],
-    tx: ['manager', 'addToQueue', 'emitMined'],
+    registry: ['profileExists', 'registerProfile', 'getCurrentProfile', 'getByAddress'],
+    tx: ['addToQueue', 'emitMined'],
 };
 const processes = ['server', 'client'];
 const mem = os_1.totalmem().toLocaleString();
@@ -29,7 +29,7 @@ Object.keys(channels).forEach((attr) => {
     channels[attr].forEach((endpoint) => {
         processes.forEach((proc) => {
             if (!EVENTS[proc].hasOwnProperty(attr)) {
-                EVENTS[proc][attr] = {};
+                EVENTS[proc][attr] = { manager: hashPath(proc, attr, mem, 'manager') };
             }
             EVENTS[proc][attr][endpoint] = hashPath(proc, attr, mem, endpoint);
         });
