5748e2cc279fb339f302ebf311ec5f71dfa6f3e7
Marius Darila <marius.darila@gmail.com>
2016-09-29 14:52:08 +0300

    fix(ipc): add api preloader for disabled nodeIntegration

--- main/lib/channels.ts
+++ main/lib/channels.ts
@@ -9,26 +9,26 @@ const hashPath = (...path: string[]) => {
 };
 const channels = {
 
-    auth: ['manager', 'login', 'logout', 'requestEther', 'generateEthKey', 'getLocalIdentities'],
+    auth: ['login', 'logout', 'requestEther', 'generateEthKey', 'getLocalIdentities'],
 
-    tags: ['manager', 'create', 'exists', 'getTagId', 'getTagAt', 'isSubscribed', 'subscribe',
+    tags: ['create', 'exists', 'getTagId', 'getTagAt', 'isSubscribed', 'subscribe',
         'unsubscribe', 'getSubPosition'],
 
-    entry: ['manager', 'publish', 'update', 'upvote', 'downvote', 'isOpenedToVotes', 'getVoteOf',
+    entry: ['publish', 'update', 'upvote', 'downvote', 'isOpenedToVotes', 'getVoteOf',
     'getVoteEndDate', 'getScore', 'getEntriesCount', 'getEntryOf', 'getEntry'],
 
-    comments: ['manager', 'publish', 'update', 'upvote', 'downvote', 'getScore'],
+    comments: ['publish', 'update', 'upvote', 'downvote', 'getScore'],
 
-    geth: ['manager', 'options', 'startService', 'stopService', 'restartService', 'syncStatus', 'logs', 'status'],
+    geth: ['options', 'startService', 'stopService', 'restartService', 'syncStatus', 'logs', 'status'],
 
-    ipfs: ['manager', 'startService', 'stopService', 'status', 'resolve', 'getConfig', 'setPorts', 'getPorts'],
+    ipfs: ['startService', 'stopService', 'status', 'resolve', 'getConfig', 'setPorts', 'getPorts'],
 
-    profile: ['manager', 'getProfileData', 'getMyBalance', 'getIpfs', 'unregister', 'follow', 'getFollowersCount',
+    profile: ['getProfileData', 'getMyBalance', 'getIpfs', 'unregister', 'follow', 'getFollowersCount',
     'getFollowingCount', 'getFollowers', 'getFollowing'],
 
-    registry: ['manager', 'profileExists', 'registerProfile', 'getCurrentProfile', 'getByAddress'],
+    registry: ['profileExists', 'registerProfile', 'getCurrentProfile', 'getByAddress'],
 
-    tx: ['manager', 'addToQueue', 'emitMined'],
+    tx: ['addToQueue', 'emitMined'],
 };
 
 const processes = ['server', 'client'];
@@ -38,7 +38,7 @@ Object.keys(channels).forEach((attr) => {
     channels[attr].forEach((endpoint: string) => {
         processes.forEach((proc) => {
             if (!EVENTS[proc].hasOwnProperty(attr)) {
-                EVENTS[proc][attr] = {};
+                EVENTS[proc][attr] = {manager: hashPath(proc, attr, mem, 'manager')};
             }
             EVENTS[proc][attr][endpoint] = hashPath(proc, attr, mem, endpoint);
         });
--- main/preloader.ts
+++ main/preloader.ts
@@ -1,3 +1,27 @@
 import Channel from './lib/channels';
+import { ApiRequest, ApiListener } from './ipcPreloader';
+import IpcRendererEventListener = Electron.IpcRendererEventListener;
 
-window['Channel'] = Channel;
+function injectApi() {
+    const AkashaApi = Object.assign({}, Channel);
+
+    Object.keys(Channel.client).forEach((module) => {
+        Object.keys(Channel.client[module]).forEach((method) => {
+            AkashaApi.client[module][method] = new ApiListener(Channel.client[module][method], method);
+        });
+    });
+
+    Object.keys(Channel.server).forEach((module) => {
+        Object.keys(Channel.server[module]).forEach((method) => {
+            if(method !== 'manager'){
+                AkashaApi.server[module][method] = new ApiRequest(
+                    Channel.server[module][method],
+                    Channel.server[module]['manager'],
+                    method
+                )
+            }
+        });
+    });
+    return AkashaApi;
+}
+global['Channel'] = injectApi();
