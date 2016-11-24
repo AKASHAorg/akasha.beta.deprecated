7a83bcf98165da2d36e258a7bd1a8992db92c650
Marius Darila <marius.darila@gmail.com>
2016-11-24 17:39:31 +0200

    feat(getEntry): resolve publisher also

--- main/lib/ipc/modules/entry/get-entry.ts
+++ main/lib/ipc/modules/entry/get-entry.ts
@@ -2,7 +2,7 @@ import * as Promise from 'bluebird';
 import { constructed as contracts } from '../../contracts/index';
 import { getShortContent, getFullContent } from './ipfs';
 import { generalSettings, BASE_URL } from '../../config/settings';
-
+import getProfileData from '../profile/profile-data';
 /**
  * Fetch entry from entryId
  * @type {Function}
@@ -11,6 +11,7 @@ const execute = Promise.coroutine(function*(data: EntryGetRequest) {
     const entryEth = yield contracts.instance.entries.getEntry(data.entryId);
     const active = yield contracts.instance.entries.isMutable(data.entryId);
     const score = yield contracts.instance.votes.getScore(data.entryId);
+    entryEth.publisher = yield getProfileData.execute({profile: entryEth.publisher});
     const content = (data.full) ? yield getFullContent(entryEth.ipfsHash) : yield getShortContent(entryEth.ipfsHash);
     return { [BASE_URL]: generalSettings.get(BASE_URL), content, entryEth, entryId: data.entryId, score, active };
 });
