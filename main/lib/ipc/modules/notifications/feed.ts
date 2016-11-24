17389f29a6032a13894c97058398928d35489613
Marius Darila <marius.darila@gmail.com>
2016-11-24 20:04:31 +0200

    feat(notifications): resolve author of event

--- main/lib/ipc/modules/notifications/feed.ts
+++ main/lib/ipc/modules/notifications/feed.ts
@@ -2,6 +2,7 @@ import * as Promise from 'bluebird';
 import { constructed as contracts } from '../../contracts/index';
 import { filter } from './set-filter';
 import { GethConnector } from '@akashaproject/geth-connector';
+import getProfileData from '../profile/profile-data';
 
 let entries;
 let comments;
@@ -10,6 +11,17 @@ const eventTypes = {
     VOTE: 'vote',
     COMMENT: 'comment',
     PUBLISH: 'publish'
+};
+
+const hydrateWithProfile = (cb, profile, extra) => {
+    getProfileData
+        .execute({ profile: profile })
+        .then((authorData) => {
+            cb('', Object.assign(extra, { author: authorData }))
+        })
+        .catch((error) => {
+            cb({ message: error.message }, extra)
+        });
 };
 /**
  * Get total number of your follows
@@ -39,38 +51,60 @@ const execute = Promise.coroutine(function*(data: { stop?: boolean }, cb) {
     votes = contracts.instance.votes.contract.Vote({}, filterBlock);
 
     entries.watch((err, entry) => {
+        if (err) {
+            cb({ message: err.message, type: eventTypes.PUBLISH });
+        }
         if (filter.hasAddress(entry.args.author)) {
-            cb(err, {
-                type: eventTypes.PUBLISH,
-                profileAddress: entry.args.author,
-                entryId: (entry.args.entryId).toString(),
-                blockNumber: entry.blockNumber,
-                tag: GethConnector.getInstance().web3.toUtf8(entry.args.tag)
-            });
+            hydrateWithProfile(
+                cb,
+                entry.args.author,
+                {
+                    type: eventTypes.PUBLISH,
+                    profileAddress: entry.args.author,
+                    entryId: (entry.args.entryId).toString(),
+                    blockNumber: entry.blockNumber,
+                    tag: GethConnector.getInstance().web3.toUtf8(entry.args.tag)
+                }
+            );
         }
     });
 
     comments.watch((err, comment) => {
+        if (err) {
+            cb({ message: err.message, type: eventTypes.COMMENT });
+        }
         if (filter.hasAddress(comment.args.profile)) {
-            cb(err, {
-                type: eventTypes.COMMENT,
-                profileAddress: comment.args.profile,
-                entryId: (comment.args.entryId).toString(),
-                blockNumber: comment.blockNumber,
-                commentId: (comment.args.commentId).toString()
-            });
+
+            hydrateWithProfile(
+                cb,
+                comment.args.profile,
+                {
+                    type: eventTypes.COMMENT,
+                    profileAddress: comment.args.profile,
+                    entryId: (comment.args.entryId).toString(),
+                    blockNumber: comment.blockNumber,
+                    commentId: (comment.args.commentId).toString()
+                }
+            );
         }
     });
 
     votes.watch((err, vote) => {
+        if (err) {
+            cb({ message: err.message, type: eventTypes.VOTE });
+        }
         if (filter.hasAddress(vote.args.profile)) {
-            cb(err, {
-                type: eventTypes.VOTE,
-                profileAddress: vote.args.profile,
-                entryId: (vote.args.entry).toString(),
-                blockNumber: vote.blockNumber,
-                weight: (vote.args.weight).toNumber()
-            });
+            hydrateWithProfile(
+                cb,
+                vote.args.profile,
+                {
+                    type: eventTypes.VOTE,
+                    profileAddress: vote.args.profile,
+                    entryId: (vote.args.entry).toString(),
+                    blockNumber: vote.blockNumber,
+                    weight: (vote.args.weight).toNumber()
+                }
+            );
         }
     });
     return { running: true }
