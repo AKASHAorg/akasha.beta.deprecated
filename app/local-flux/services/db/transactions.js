78312fcba8bf8a5dfb27e962196fe583c59d456f
Marius Darila <marius.darila@gmail.com>
2016-11-29 15:53:52 +0200

    feat(db): add scope to db name

--- app/local-flux/services/db/transactions.js
+++ app/local-flux/services/db/transactions.js
@@ -1,6 +1,7 @@
 import Dexie from 'dexie';
 
-const transactionsDB = new Dexie('transactions');
+const dbName = (process.env.NODE_ENV === 'production') ? 'transactions-akasha': 'transactions-dev';
+const transactionsDB = new Dexie(dbName);
 transactionsDB.version(1).stores({
     pending: '&tx, type, profile, [type+profile]',
     mined: '&tx, type, profile, [type+profile]',
