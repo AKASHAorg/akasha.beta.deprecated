88301c8d55426f2b10c4953b73d230e3ac418261
Marius Darila <marius.darila@gmail.com>
2016-12-06 16:25:00 +0200

    feat(tags): add search tag channel

--- main/lib/channels.ts
+++ main/lib/channels.ts
@@ -12,7 +12,7 @@ const channels = {
     auth: ['login', 'logout', 'requestEther', 'generateEthKey', 'getLocalIdentities'],
 
     tags: ['checkFormat', 'create', 'tagIterator', 'tagSubIterator', 'exists', 'getTagsCreated', 'subsCount',
-        'subscribe', 'getTagId', 'getTagName', 'unSubscribe', 'isSubscribed'],
+        'subscribe', 'getTagId', 'getTagName', 'unSubscribe', 'isSubscribed', 'searchTag'],
 
     entry: ['getProfileEntriesCount', 'getTagEntriesCount', 'isActive', 'getEntry', 'publish', 'update', 'canClaim', 'claim',
         'downvote', 'getScore', 'getDepositBalance', 'upvote', 'voteCost', 'voteCount', 'entryTagIterator',
