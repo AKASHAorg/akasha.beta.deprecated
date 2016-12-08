eebdc920c29f51d77e7dcd8a2dc00db22fe1c2fc
Marius Darila <marius.darila@gmail.com>
2016-12-08 13:38:19 +0200

    feat(comments): resolve comment author

--- main/lib/ipc/modules/comments/get-comment.ts
+++ main/lib/ipc/modules/comments/get-comment.ts
@@ -1,15 +1,18 @@
 import * as Promise from 'bluebird';
 import { constructed as contracts } from '../../contracts/index';
 import { getCommentContent } from './ipfs';
+import getProfile from '../profile/profile-data';
 
 /**
- * Get total number of comments for an entry
+ * Get comment data for an entry
  * @type {Function}
  */
 const execute = Promise.coroutine(function*(data: { entryId: string, commentId: string}) {
     const ethData = yield contracts.instance.comments.getComment(data.entryId, data.commentId);
+    const profile = yield getProfile.execute({profile: ethData.profile});
     const content = yield getCommentContent(ethData.ipfsHash);
-    return { content: Object.assign(ethData, content), entryId: data.entryId, commentId: data.commentId };
+    ethData.profile = profile;
+    return { data: Object.assign(ethData, content), entryId: data.entryId, commentId: data.commentId };
 });
 
 export default { execute, name: 'getComment' };
