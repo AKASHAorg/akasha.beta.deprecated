73ca7fa517549356f161b64f556b8b941c7f6b08
Bogdan Brezuica <bogdan.brezuica@akasha.org>
2016-12-14 12:42:03 +0200

    fix(entry content): Remove draft slice

--- app/routes/Home/routes/Entry/components/entry-page-content.js
+++ app/routes/Home/routes/Entry/components/entry-page-content.js
@@ -6,8 +6,7 @@ import styles from './entry-page-content.scss';
 class EntryPageContent extends Component {
     render () {
         const { entry } = this.props;
-        const cleanupEntry = entry.getIn(['content']).toJS().draft.slice(5, entry.get('content').toJS().draft.length - 3);
-        const entryContent = editorStateFromRaw(JSON.parse(cleanupEntry));
+        const entryContent = editorStateFromRaw(entry.getIn(['content', 'draft']).toJS());
         return (
           <div className={`${styles.content_inner} row`} >
             <div className="col-xs-12">
