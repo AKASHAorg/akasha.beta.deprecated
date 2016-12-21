e458e713b94c1ee0f98b3a078c723c0254ba098c
Sever Abibula <severs@akasha.world>
2016-12-21 19:10:25 +0200

    cleanup(routes): cleanup routes file

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, IndexRoute, IndexRedirect } from 'react-router';
+import { Route, IndexRoute } from 'react-router';
 import AppContainer from './routes/AppContainer';
 import BootAppContainer from './routes/BootApp/BootAppContainer';
 import SetupContainer from './routes/Setup/SetupContainer';
@@ -25,61 +25,28 @@ import requireAuth from './require-auth';
 export default (
   <Route component={AppContainer} path="/" > {/** displays errors and various info */}
 
-    {/** checks for userSettings, if geth is running ... */}
     <IndexRoute component={BootAppContainer} />
-
-    {/** checks if first start or geth/ipfs already configured*/}
     <Route component={SetupContainer} path="setup" >
-
-      {/** configures geth and ipfs */}
       <IndexRoute component={ConfigurationContainer} />
-
-      {/** syncs blockchain*/}
       <Route component={SynchronizationContainer} path="sync-status" />
     </Route>
-
-    {/** check if user is logged in*/}
     <Route component={LoginLayout} path="authenticate">
-
-      {/** shows local profiles and login */}
       <IndexRoute component={AuthContainer} />
-
-      {/** form for profile creation */}
       <Route component={CreateProfileContainer} path="new-profile" />
-
-      {/** display the status of profile creation */}
       <Route component={CreateProfileStatusContainer} path="new-profile-status" />
-
-      {/** display info about profile on completion - error or success */}
       <Route component={CreateProfileCompleteContainer} path="new-profile-complete" />
     </Route>
-
-    {/** user home after login */}
     <Route component={requireAuth(HomeContainer)} path=":akashaId" >
-      {/*<IndexRedirect to="explore/tag" ignoreScrollBehaviour />*/}
-
-      {/** loads articles from blockchain */}
       <Route component={StreamPageContainer}>
         <Route component={EntryListContainer} path="explore(/:filter)" />
       </Route>
-
-      {/** loads details about different profiles */}
       <Route component={PeoplePageContainer} path="people" />
       <Route component={ProfileDetailsContainer} path="profile/:profileAddress" />
-
-      {/** create a new entry or edit existing one */}
       <Route component={NewEntryContainer} path="draft/:draftId" >
-
-        {/** publish an entry */}
         <Route component={PublishEntryPanelContainer} path="publish" />
-
-        {/** display status of the publishing step */}
         <Route component={PublishEntryStatusContainer} path="publish-status" />
-
-        {/** display info about newly published entry */}
         <Route component={PublishEntryCompleteContainer} path="publish-complete" />
       </Route>
-      {/** view entry page */}
       <Route component={EntryPageContainer} path="entry/:entryId" />
     </Route>
   </Route>
