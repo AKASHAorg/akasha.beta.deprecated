24743b66c9d4b80a77e2871efe7a2ee2c31ef4b1
Marius Darila <marius.darila@gmail.com>
2017-01-13 14:09:32 +0200

    refactor(entry-iterator): add parallel calls

--- main/lib/ipc/modules/entry/downvote-entry.js
+++ main/lib/ipc/modules/entry/downvote-entry.js
@@ -2,14 +2,12 @@
 const index_1 = require('../auth/index');
 const Promise = require('bluebird');
 const index_2 = require('../../contracts/index');
-const runner_1 = require('../pinner/runner');
 const execute = Promise.coroutine(function* (data) {
     if (data.weight < 1 || data.weight > 10) {
         throw new Error("Vote weight value must be between 1-10");
     }
     const txData = yield index_2.constructed.instance.votes.downvote(data.entryId, data.weight, data.value, data.gas);
     const tx = yield index_1.module.auth.signData(txData, data.token);
-    runner_1.default.execute({ type: runner_1.ObjectType.ENTRY, id: data.entryId, operation: runner_1.OperationType.REMOVE });
     return { tx, entryId: data.entryId, extra: data.extra };
 });
 Object.defineProperty(exports, "__esModule", { value: true });
--- main/lib/ipc/modules/entry/entry-profile-iterator.ts
+++ main/lib/ipc/modules/entry/entry-profile-iterator.ts
@@ -5,18 +5,16 @@ import getEntry from './get-entry';
  * Get entries indexed by publisher
  * @type {Function}
  */
-const execute = Promise.coroutine(function*(data: {start?: number, limit?: number, akashaId: string }) {
+const execute = Promise.coroutine(function*(data: { start?: number, limit?: number, akashaId: string }) {
     let currentId = (data.start) ? data.start : yield contracts.instance.entries.getProfileEntryFirst(data.akashaId);
     if (currentId === '0') {
         return { collection: [], akashaId: data.akashaId };
     }
-    let entry;
     const maxResults = (data.limit) ? data.limit : 5;
-    const results = [];
+    const fetchCalls = [];
     let counter = 0;
     if (!data.start) {
-        entry = yield getEntry.execute({ entryId: currentId });
-        results.push({ entryId: currentId, content: entry });
+        fetchCalls.push(getEntry.execute({ entryId: currentId }));
         counter = 1;
     }
 
@@ -25,10 +23,11 @@ const execute = Promise.coroutine(function*(data: {start?: number, limit?: numbe
         if (currentId === '0') {
             break;
         }
-        entry = yield getEntry.execute({ entryId: currentId });
-        results.push({ entryId: currentId, content: entry });
+        fetchCalls.push(getEntry.execute({ entryId: currentId }));
         counter++;
     }
+    const results = yield Promise.all(fetchCalls);
+
     return { collection: results, akashaId: data.akashaId, limit: maxResults };
 });
 
