09f69ecc05d3e41206c37c5a4174a5cb5af7edc0
Marius Darila <marius.darila@gmail.com>
2017-01-16 13:39:35 +0200

    refactor(send-tip): add strict validation for receiver

--- main/lib/ipc/modules/profile/send-tip.js
+++ main/lib/ipc/modules/profile/send-tip.js
@@ -2,10 +2,15 @@
 const Promise = require('bluebird');
 const index_1 = require('../../contracts/index');
 const index_2 = require('../auth/index');
+const address_of_akashaid_1 = require('../registry/address-of-akashaid');
 const execute = Promise.coroutine(function* (data) {
+    const validateReceiver = yield address_of_akashaid_1.default.execute([{ akashaId: data.akashaId }]);
+    if (validateReceiver.collection[0] !== data.receiver) {
+        throw new Error("Cannot validate receiver's address.");
+    }
     const txData = yield index_1.constructed.instance.profile.sendTip(data.receiver, data.value, data.unit, data.gas);
     const tx = yield index_2.module.auth.signData(txData, data.token);
-    return { tx, receiver: data.receiver };
+    return { tx, receiver: data.receiver, akashaId: data.akashaId };
 });
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = { execute, name: 'tip' };
