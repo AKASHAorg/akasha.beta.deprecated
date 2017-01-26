c57375d63adce03095b2a00b3d3bf3c5c038268b
Marius Darila <marius.darila@gmail.com>
2017-01-26 14:25:53 +0200

    refactor(followingStream): return last entry block

--- main/lib/ipc/modules/chat/join.ts
+++ main/lib/ipc/modules/chat/join.ts
@@ -6,15 +6,15 @@ const execute = Promise.coroutine(function*(data: { channels: string[] }) {
     if (!data.channels || !data.channels.length) {
         throw new Error('Must provide at least a channel');
     }
-    const maxChannels = 10;
+    //const maxChannels = 10;
     data.channels.forEach((chan) => {
-        if (settings.TOPICS.size === maxChannels) {
+        /**if (settings.TOPICS.size === maxChannels) {
             throw new Error(`Max ${maxChannels} channels reached`);
-        }
+        }**/
         settings.TOPICS.add(GethConnector.getInstance().web3.fromUtf8(chan));
     });
 
-    return { channels: data.channels, numChannels: settings.TOPICS.size, maxChannels };
+    return { channels: data.channels, numChannels: settings.TOPICS.size };
 });
 
 export default { execute, name: 'join' };
