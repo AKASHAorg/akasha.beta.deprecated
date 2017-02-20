98bb8bf9c824fbff59e97595d8e1b62e383a77a0
Marius Darila <marius.darila@gmail.com>
2017-02-20 13:46:45 +0200

    chore(): typo

--- main/lib/ipc/modules/profile/get-profile-list.ts
+++ main/lib/ipc/modules/profile/get-profile-list.ts
@@ -8,7 +8,7 @@ import getProfileData from "./profile-data";
 const execute = Promise.coroutine(function*(data: ProfileDataRequest[]) {
 
     const pool = data.map((profile) => {
-        return getProfileData.executeri(profile);
+        return getProfileData.execute(profile);
     });
     const collection = yield Promise.all(pool);
     return { collection: collection, resolve: data };
