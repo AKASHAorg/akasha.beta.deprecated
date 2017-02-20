895bdb70bf8843bfdacd1a2a8ff604c789e418bc
Marius Darila <marius.darila@gmail.com>
2017-02-20 13:46:45 +0200

    chore(): typo

--- main/lib/ipc/modules/profile/get-profile-list.js
+++ main/lib/ipc/modules/profile/get-profile-list.js
@@ -3,7 +3,7 @@ const Promise = require("bluebird");
 const profile_data_1 = require("./profile-data");
 const execute = Promise.coroutine(function* (data) {
     const pool = data.map((profile) => {
-        return profile_data_1.default.executeri(profile);
+        return profile_data_1.default.execute(profile);
     });
     const collection = yield Promise.all(pool);
     return { collection: collection, resolve: data };
