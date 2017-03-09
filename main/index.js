43e16c1b6066dcde9e3854b9eeefdeab22b766c5
Marius Darila <marius.darila@gmail.com>
2017-03-09 18:31:54 +0200

    chore(): prep for 0.5.2

--- main/index.js
+++ main/index.js
@@ -62,6 +62,7 @@ function bootstrapApp() {
             resizable: true,
             show: false,
             webPreferences: {
+                nodeIntegration: false,
                 preload: path_1.resolve(__dirname, 'preloader.js')
             }
         });
