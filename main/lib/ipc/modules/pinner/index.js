af14b138ad7da1154841e4eda186198205bc58cc
Marius Darila <marius.darila@gmail.com>
2017-04-21 13:31:11 +0300

    chore()

--- main/lib/ipc/modules/pinner/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const runner_1 = require("./runner");
-exports.default = [runner_1.default];
-//# sourceMappingURL=index.js.map
