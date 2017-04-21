98241ea467565289c5d08635a153efc6d58d0440
Marius Darila <marius.darila@gmail.com>
2017-04-21 13:32:16 +0300

    chore(): ts

--- main/typings/main.d.ts
+++ main/typings/main.d.ts
@@ -1,2 +1,2 @@
 /// <reference path="main/definitions/ipc-data-types/index.d.ts" />
-/// <reference path="main/definitions/@akashaproject/contracts.js/index.d.ts" />
+/// <reference path="main/definitions/@akashaproject/contracts-js/index.d.ts" />
