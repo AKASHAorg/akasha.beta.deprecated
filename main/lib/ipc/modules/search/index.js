827b5ff64ec927218f29bdfe183928382d9885fd
Marius Darila <marius.darila@gmail.com>
2017-04-21 13:31:11 +0300

    chore()

--- main/lib/ipc/modules/search/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const handshake_1 = require("./handshake");
-const query_1 = require("./query");
-exports.default = [handshake_1.default, query_1.default];
-//# sourceMappingURL=index.js.map
--- main/lib/ipc/modules/utils/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const backup_keystore_1 = require("./backup-keystore");
-exports.default = [backup_keystore_1.default];
-//# sourceMappingURL=index.js.map
