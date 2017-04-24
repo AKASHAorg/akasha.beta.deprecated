cd49f4b0f412f3d2323201bbce0d8f981385433a
Sever Abibula <sever.abibula@akasha.org>
2017-04-24 19:38:26 +0300

    chore(): moving the commit

--- app/containers/new-profile-container.js
+++ app/containers/new-profile-container.js
@@ -1,6 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { tempProfileUpdate, tempProfileCreate } from '../local-flux/actions/temp-profile-actions';
 import { NewProfileForm } from '../components';
+
+const submitForm = props => (profileData) => {
+    props.tempProfileCreate(profileData);
+    props.history.push('/setup/new-identity-status');
+};
+
+const cancelForm = props => () =>
+    props.history.push('/setup/authenticate');
+
+const NewProfileContainer = props =>
+  <NewProfileForm
+    onSubmit={submitForm(props)}
+    onCancel={cancelForm(props)}
+  />;
+
+NewProfileContainer.propTypes = {
+    tempProfileCreate: PropTypes.func,
+    history: PropTypes.shape()
+};
 
 function mapStateToProps (state) {
     return {
@@ -14,6 +35,6 @@ export default connect(
         tempProfileCreate,
         tempProfileUpdate
     }
-)(NewProfileForm);
+)(NewProfileContainer);
 
-export { NewProfileForm };
+export { NewProfileContainer };
