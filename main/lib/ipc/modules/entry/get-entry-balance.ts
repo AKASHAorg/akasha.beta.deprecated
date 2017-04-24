a90e7594427d340add23713258a55aa84e806699
Marius Darila <marius.darila@gmail.com>
2017-04-24 17:28:24 +0300

    refactor(main:req): add handler for list params

--- main/lib/ipc/modules/entry/get-entry-balance.ts
+++ main/lib/ipc/modules/entry/get-entry-balance.ts
@@ -6,15 +6,34 @@ import { GethConnector } from '@akashaproject/geth-connector';
  * Get current balance of an entry
  * @type {Function}
  */
-const execute = Promise.coroutine(function*(data: {entryId: string}) {
-    const balanceAddress = yield contracts.instance.entries.getEntryFund(data.entryId);
-    if (!balanceAddress) {
-        return { balance: 'claimed', unit: '', entryId: data.entryId };
-    }
-    const weiAmount = yield GethConnector.getInstance().web3.eth.getBalanceAsync(balanceAddress);
-    const balance = GethConnector.getInstance().web3.fromWei(weiAmount, 'ether');
+const execute = Promise.coroutine(
+    /**
+     *
+     * @param data
+     * @returns {{collection: any}}
+     */
+    function*(data: { entryId: string[], unit: 'ether' }) {
+        if (!Array.isArray(data.entryId)) {
+            throw new Error('data.entryId must be an array');
+        }
 
-    return { balance: balance.toString(10), unit: 'ether', entryId: data.entryId };
-});
+        const requests = data.entryId.map((id) => {
+            return contracts.instance.entries
+                .getEntryFund(id)
+                .then((balanceAddress) => {
+                    if (!balanceAddress) {
+                        return { balance: 'claimed', unit: data.unit, entryId: id };
+                    }
+                    return GethConnector.getInstance().web3.eth.getBalanceAsync(balanceAddress)
+                        .then((weiAmount) => {
+                            const balance = GethConnector.getInstance().web3.fromWei(weiAmount, data.unit);
+                            return { balance: balance.toString(10), unit: data.unit, entryId: id }
+                        })
+                });
+        });
+
+        const collection = yield Promise.all(requests);
+        return { collection };
+    });
 
 export default { execute, name: 'getEntryBalance' };
