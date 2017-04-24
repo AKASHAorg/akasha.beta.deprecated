ebb5dab09b1335ccff150bbdfbb848c69d616aa6
Marius Darila <marius.darila@gmail.com>
2017-04-24 17:28:24 +0300

    refactor(main:req): add handler for list params

--- main/lib/ipc/modules/entry/get-vote-of.ts
+++ main/lib/ipc/modules/entry/get-vote-of.ts
@@ -6,11 +6,29 @@ import { constructed as contracts } from '../../contracts/index';
  * Get vote weight of akasha id
  * @type {Function}
  */
-const execute = Promise.coroutine(function*(data: {entryId: string, akashaId: string}) {
-    const profileAddress = yield contracts.instance.registry.addressOf(data.akashaId);
-    const weight = yield contracts.instance.votes.getVoteOfProfile(data.entryId, profileAddress);
-    return { weight, entryId: data.entryId, akashaId: data.akashaId };
-});
+const execute = Promise.coroutine(
+    /**
+     *
+     * @param data
+     * @returns {{collection: any}}
+     */
+    function*(data: { entryId: string, akashaId: string }[]) {
+        if(!Array.isArray(data)){
+            throw new Error('data must be an array');
+        }
+        const requests = data.map((req) => {
+            return contracts.instance.registry.addressOf(req.akashaId)
+                .then((profileAddress) => {
+                    return contracts.instance.votes.getVoteOfProfile(req.entryId, profileAddress)
+                })
+                .then((weight) => {
+                    return { weight, entryId: req.entryId, akashaId: req.akashaId }
+                });
+        });
+
+        const collection = yield Promise.all(requests);
+        return { collection };
+    });
 
 export default { execute, name: 'getVoteOf' };
 
