173238b77b51ac4f0b361edf97de993273a1a42e
Vali Cotea <quinine35@gmail.com>
2017-05-05 14:51:57 +0300

    refactor(utils): remove redundant yields

--- main/lib/ipc/modules/utils/os-info.ts
+++ main/lib/ipc/modules/utils/os-info.ts
@@ -2,20 +2,17 @@ import * as Promise from 'bluebird';
 import { platform, arch, type } from 'os';
 
 
-const execute = Promise.coroutine(function*(data:{ info?: string }){
-    const platformInfo = yield Promise.resolve(platform());
-    const archInfo = yield Promise.resolve(arch());
-    const typeInfo = yield Promise.resolve(type());
-    const memory = yield Promise.resolve(process.getProcessMemoryInfo());
-    return {platform: {
-                platform: platformInfo,
-                arch: archInfo,
-                type: typeInfo
+const execute = Promise.coroutine(function*(){
+    let info = yield Promise.resolve({platform: {
+                platform: platform(),
+                arch: arch(),
+                type: type()
             },
             resources: {
-                memoryUsage: memory
+                memoryUsage: process.getProcessMemoryInfo()
             }
-        };
+        });
+    return info;
 });
 
 export default { execute, name: 'osInfo'};
