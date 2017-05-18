86b13b55ddef3f66d608ef3bd6b8be9d4183cc1c
Marius Darila <marius.darila@gmail.com>
2017-05-18 18:04:02 +0300

    refactor(startup): add better bootrap services flow

--- main/init-modules.ts
+++ main/init-modules.ts
@@ -1,50 +1,24 @@
 import { checkForGenesis } from './config/genesis';
-import GethIPC from './modules/GethIPC';
-import IpfsIPC from './modules/IpfsIPC';
-import AuthIPC from './modules/AuthIPC';
+import * as Promise from 'bluebird';
 import Logger from './modules/Logger';
-import TxIPC from './modules/TxIPC';
-import RegistryIPC from './modules/RegistryIPC';
-import ProfileIPC from './modules/ProfileIPC';
-import TagsIPC from './modules/TagsIPC';
-import EntryIPC from './modules/EntryIPC';
-import CommentsIPC from './modules/CommentsIPC';
-import LicensesIPC from './modules/LicensesIPC';
-import NotificationsIPC from './modules/NotificationsIPC';
-import ChatIPC from './modules/ChatIPC';
-import SearchIPC from './modules/SearchIPC';
-import UtilsIPC from './modules/UtilsIPC';
-
+import ipcChannels from './modules';
 import WebContents = Electron.WebContents;
 
 export function initModules() {
     const logger = Logger.getInstance();
-    const ipcChannels: any[] = [
-        new GethIPC(),
-        new IpfsIPC(),
-        new AuthIPC(),
-        new TxIPC(),
-        new RegistryIPC(),
-        new ProfileIPC(),
-        new TagsIPC(),
-        new EntryIPC(),
-        new CommentsIPC(),
-        new LicensesIPC(),
-        new NotificationsIPC(),
-        new ChatIPC(),
-        new SearchIPC(),
-        new UtilsIPC()
-    ];
     return {
         initListeners: (webContents: WebContents) => {
-            logger.registerLogger('akasha', { maxsize: 50 * 1024 });
-            ipcChannels.forEach((obj: any) => {
-                obj.initListeners(webContents);
-            });
-            checkForGenesis((errGenesis) => {
-                if (errGenesis) {
-                    (Logger.getInstance().getLogger('akasha')).error(errGenesis);
-                }
+            return new Promise((resolve) => {
+                logger.registerLogger('akasha', { maxsize: 50 * 1024 });
+                ipcChannels.forEach((obj: any) => {
+                    obj.initListeners(webContents);
+                });
+                checkForGenesis((errGenesis) => {
+                    if (errGenesis) {
+                        (Logger.getInstance().getLogger('akasha')).error(errGenesis);
+                    }
+                    resolve();
+                });
             });
         },
         logger,
