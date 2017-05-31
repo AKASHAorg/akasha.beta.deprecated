97d194cc72ca8300cae83437c8b0e79ebbdb945c
Vali Cotea <quinine35@gmail.com>
2017-05-31 12:09:52 +0300

    feat(auth): get-local-identities returns akashaId

--- main/modules/auth/get-local-identities.ts
+++ main/modules/auth/get-local-identities.ts
@@ -1,8 +1,12 @@
 import * as Promise from 'bluebird';
-import { constructed } from '../../contracts/index';
+import { constructed as contracts } from '../../contracts/index';
 
 const execute = Promise.coroutine(function*() {
-    return constructed.instance.registry.getLocalProfiles();
+    const profiles = contracts.instance.registry.getLocalProfiles();
+    for (let profile of profiles) {
+        profile.akashaId = yield contracts.instance.profile.getId(profile.profile);
+    }
+    return profiles;
 });
 
 export default { execute, name: 'getLocalIdentities' };
