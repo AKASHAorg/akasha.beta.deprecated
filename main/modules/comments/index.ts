233c4c46f0fdb2aca4a08da05de0939e15d8b976
Vali Cotea <quininez@users.noreply.github.com>
2017-06-01 17:08:51 +0300

    Vali (#279)
    * feat(comments): comment iterator showing latest comments and their replies
    
    * wip
    
    * rebase from develop
    
    * wip
    
    * feat(comments): finalized comments-parent iterator, get-profile-comments, resolve-comments-ipfs-hash
    
    * minor fix

--- main/modules/comments/index.ts
+++ main/modules/comments/index.ts
@@ -3,11 +3,17 @@ import commentsCount from './comments-count';
 import getComment from './get-comment';
 import removeComment from './remove-comment';
 import commentsIterator from './comments-iterator';
+import commentsParentIterator from './comments-parent-iterator';
+import getProfileComments from './get-profile-comments';
+import resolveCommentsIpfsHash from './resolve-comments-ipfs-hash';
 
 export default [
     addComment,
     commentsCount,
     getComment,
     removeComment,
-    commentsIterator
+    commentsIterator,
+    commentsParentIterator,
+    getProfileComments,
+    resolveCommentsIpfsHash
 ];
