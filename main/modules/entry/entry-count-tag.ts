d9fa0efdcb0773c5a45a0ec75f8a7b4a29297160
Marius Darila <marius.darila@gmail.com>
2017-06-27 18:08:32 +0300

    refactor(tagEntriesCount): handle array request

--- main/modules/entry/entry-count-tag.ts
+++ main/modules/entry/entry-count-tag.ts
@@ -5,9 +5,16 @@ import { constructed as contracts } from '../../contracts/index';
  * Get total number of entries posted by profile
  * @type {Function}
  */
-const execute = Promise.coroutine(function*(data: EntriesCountTagRequest) {
-    const count = yield contracts.instance.entries.getTagEntriesCount(data.tagName);
-    return { count, tagName: data.tagName };
+const execute = Promise.coroutine(function*(data: EntriesCountTagRequest []) {
+    const requests = data.map((tag) => {
+        return contracts.instance.entries
+            .getTagEntriesCount(tag.tagName)
+            .then((count) => {
+                return { count, tagName: tag.tagName };
+            });
+    });
+    const collection = yield Promise.all(requests);
+    return { collection };
 });
 
 export default { execute, name: 'getTagEntriesCount' };
