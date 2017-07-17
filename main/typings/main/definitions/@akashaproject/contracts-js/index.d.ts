bdc21ad7e72137a80397d005f65701a0a91b1ffd
Marius Darila <marius.darila@gmail.com>
2017-07-17 19:36:49 +0300

    chore(): update deps

--- main/typings/main/definitions/@akashaproject/contracts-js/index.d.ts
+++ main/typings/main/definitions/@akashaproject/contracts-js/index.d.ts
@@ -5,32 +5,33 @@ declare module '@akashaproject/contracts.js' {
 }
 
 declare module 'ethereumjs-util' {
-    export function ecsign(msgHash: string, privateKey: string): { r: string, s: string, v: string }
+    export function ecsign(msgHash: string, privateKey: string): { r: string, s: string, v: string };
 
-    export function ecrecover(msgHash: Buffer, v: string, r: string, s: string): Buffer
+    export function ecrecover(msgHash: Buffer, v: string, r: string, s: string): Buffer;
 
-    export function fromRpcSig(sig: string): { r: string, s: string, v: string }
+    export function fromRpcSig(sig: string): { r: string, s: string, v: string };
 
-    export function isValidAddress(address: string): boolean
+    export function isValidAddress(address: string): boolean;
 
-    export function toBuffer(data: any): Buffer
+    export function toBuffer(data: any): Buffer;
 
-    export function bufferToHex(data: Buffer): string
+    export function bufferToHex(data: Buffer): string;
 
-    export function pubToAddress(pubKey: string, sanitize?: boolean): Buffer
+    export function pubToAddress(pubKey: string, sanitize?: boolean): Buffer;
 
-    export function unpad(data: Buffer | string): Buffer | string
+    export function unpad(data: Buffer | string): Buffer | string;
 
-    export function hashPersonalMessage(data: Buffer): Buffer
+    export function hashPersonalMessage(data: Buffer): Buffer;
 
-    export function addHexPrefix(data: string): string
+    export function addHexPrefix(data: string): string;
 
-    export function stripHexPrefix(data: string): string
+    export function stripHexPrefix(data: string): string;
 }
 
 
-declare module 'ethereumjs-testrpc'
+declare module 'ethereumjs-testrpc';
 declare module 'spectron';
 declare module 'compare-versions';
 declare module 'electron-spellchecker';
+declare module 'archiver';
 
