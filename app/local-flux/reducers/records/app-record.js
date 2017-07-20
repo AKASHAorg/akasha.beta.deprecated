2a7277a02b8c18dff13bfe6c8e7d9ff359b938a3
Sever Abibula (SeverS) <sever.abibula@akasha.org>
2017-07-20 18:08:11 +0300

    chore(): fix stylings, revert app-actions

--- app/local-flux/reducers/records/app-record.js
+++ app/local-flux/reducers/records/app-record.js
@@ -7,24 +7,31 @@ export const NotificationRecord = Record({
 });
 
 export const PendingActionRecord = Record({
-    // specify entity type of the resource that will get published
-    // usefull if you want to filter a specific type of entity
-    // eg: tempProfile, upvote, comment, downvote, etc
-    entityType: null,
-    // eg. publish, register, update, delete etc..
-    actionType: null,
-    // usefull to create relations between pending action and resource
-    entityId: null,
-    // status of the pending action (use actionTypes for this field)
-    currentAction: null,
-    // tx received from main
-    publishTx: null,
-    // for entities that requires confirmation (gas or other)
-    confirmed: false,
-    // triggers when the entity sent to publish on main
-    published: false,
-    // gas specified by user. if null apply default gas
-    gas: null
+    // // specify entity type of the resource that will get published
+    // // usefull if you want to filter a specific type of entity
+    // // eg: tempProfile, upvote, comment, downvote, etc
+    // entityType: null,
+    // // eg. publish, register, update, delete etc..
+    // actionType: null,
+    // // usefull to create relations between pending action and resource
+    // entityId: null,
+    // // status of the pending action (use actionTypes for this field)
+    // currentAction: null,
+    // // tx received from main
+    // publishTx: null,
+    // // for entities that requires confirmation (gas or other)
+    // confirmed: false,
+    // // triggers when the entity sent to publish on main
+    // published: false,
+    // // gas specified by user. if null apply default gas
+    // gas: null
+    id: null,
+    type: null,
+    payload: new Map(),
+    titleId: null,
+    messageId: null,
+    gas: null,
+    status: null
 });
 
 const AppRecord = Record({
