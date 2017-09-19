62e6db522949d991c3f90d12432c58d24dab9c59
Marius Darila <marius.darila@gmail.com>
2017-09-19 15:31:05 +0300

    feat(contracts): add tx send and watch

--- main/contracts/index.ts
+++ main/contracts/index.ts
@@ -1,5 +1,6 @@
 const initContracts = require('@akashaproject/contracts.js');
 import { GethConnector } from '@akashaproject/geth-connector';
+import auth from '../modules/auth/Auth';
 
 class Contracts {
     public instance: any;
@@ -12,6 +13,39 @@ class Contracts {
         this.instance = await initContracts(GethConnector.getInstance().web3.currentProvider);
         return this.instance;
     }
+
+    public async send(data: any, token: string, cb) {
+        const tx = await auth.signData(data, token);
+        cb(null, { tx });
+        return Contracts.watchTx(tx);
+    }
+
+    public static watchTx(tx: string) {
+        const timeout = 300000;
+        const start = new Date().getTime();
+        return new Promise((resolve, reject) => {
+            const getReceipt = function () {
+                GethConnector.getInstance().web3
+                    .eth.getTransactionReceipt((err, receipt) => {
+                    if (receipt != null) {
+                        return resolve({
+                            tx: tx,
+                            receipt: receipt
+                        });
+                    }
+
+                    if (new Date().getTime() - start > timeout) {
+                        return reject(new Error('Tx: ' + tx + ' timed out'));
+                    }
+
+                    setTimeout(getReceipt, 2000);
+                });
+
+            };
+            getReceipt();
+        });
+    }
+
 }
 
 export default new Contracts();
--- main/modules/profile/follow-profile.ts
+++ main/modules/profile/follow-profile.ts
@@ -1,6 +1,5 @@
 import * as Promise from 'bluebird';
 import contracts from '../../contracts/index';
-import auth from '../auth/Auth';
 import pinner, { ObjectType, OperationType } from '../pinner/runner';
 import { mixed } from '../models/records';
 
@@ -8,12 +7,22 @@ import { mixed } from '../models/records';
  * Follow an akasha profile
  * @type {Function}
  */
-const execute = Promise.coroutine(function* (data: ProfileFollowRequest) {
-    const txData = yield contracts.instance.feed.follow(data.akashaId, data.gas);
-    const tx = yield auth.signData(txData, data.token);
+const execute = Promise.coroutine(function* (data: ProfileFollowRequest, cb) {
+    let profileAddress;
+    console.log(contracts.instance.Feed.follow);
+    if (data.akashaId) {
+        profileAddress = yield contracts.instance.ProfileResolver.addr(data.akashaId);
+    } else if (data.ethAddress) {
+        profileAddress = data.ethAddress;
+    }
+    if (!profileAddress) {
+        throw new Error('Must provide an akasha ID or ethereum address');
+    }
+    const txData = yield contracts.instance.Feed.follow.request(profileAddress, { gas: 400000 });
+    const tx = yield contracts.send(txData, data.token, cb);
     mixed.flush();
     pinner.execute({ type: ObjectType.PROFILE, id: data.akashaId, operation: OperationType.ADD });
     return { tx, akashaId: data.akashaId };
 });
 
-export default { execute, name: 'followProfile' };
+export default { execute, name: 'followProfile', hasStream: true };
