e28e9ac3c4bfe38a985bf5aa7cb2ab700cf4c83e
Marius Darila <marius.darila@gmail.com>
2017-09-19 16:41:07 +0300

    wip(profiles): tmp commit

--- main/modules/profile/follow-profile.ts
+++ main/modules/profile/follow-profile.ts
@@ -2,23 +2,14 @@ import * as Promise from 'bluebird';
 import contracts from '../../contracts/index';
 import pinner, { ObjectType, OperationType } from '../pinner/runner';
 import { mixed } from '../models/records';
-
+import { profileAddress } from './helpers';
 /**
  * Follow an akasha profile
  * @type {Function}
  */
 const execute = Promise.coroutine(function* (data: ProfileFollowRequest, cb) {
-    let profileAddress;
-    console.log(contracts.instance.Feed.follow);
-    if (data.akashaId) {
-        profileAddress = yield contracts.instance.ProfileResolver.addr(data.akashaId);
-    } else if (data.ethAddress) {
-        profileAddress = data.ethAddress;
-    }
-    if (!profileAddress) {
-        throw new Error('Must provide an akasha ID or ethereum address');
-    }
-    const txData = yield contracts.instance.Feed.follow.request(profileAddress, { gas: 400000 });
+    const address = yield profileAddress(data);
+    const txData = yield contracts.instance.Feed.follow.request(address, { gas: 400000 });
     const tx = yield contracts.send(txData, data.token, cb);
     mixed.flush();
     pinner.execute({ type: ObjectType.PROFILE, id: data.akashaId, operation: OperationType.ADD });
