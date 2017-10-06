6faa6d27df080329b0a41470a61fcd3c7e7fbda7
Marius Darila <marius.darila@gmail.com>
2017-10-06 16:27:53 +0300

    feat(token): add transition states api

---
+++ main/modules/profile/aeth-free.ts
@@ -0,0 +1,26 @@
+import * as Promise from 'bluebird';
+import schema from '../utils/jsonschema';
+import contracts from '../../contracts/index';
+
+export const freeAeth = {
+    'id': '/freeAeth',
+    'type': 'object',
+    'properties': {
+        'token': { 'type': 'string' }
+    },
+    'required': ['amount', 'token']
+};
+/**
+ *
+ * @type {Function}
+ */
+const execute = Promise.coroutine(function* (data: { amount: string, token: string }, cb) {
+    const v = new schema.Validator();
+    v.validate(data, freeAeth, { throwError: true });
+
+    const txData = contracts.instance.AETH.freeAeth.request({ gas: 100000 });
+    const transaction = yield contracts.send(txData, data.token, cb);
+    return { tx: transaction.tx, receipt: transaction.receipt };
+});
+
+export default { execute, name: 'freeAeth', hasStream: true };
