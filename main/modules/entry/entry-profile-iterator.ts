411b03f1219b29d46c35bfdee924017f65f50f7b
Marius Darila <marius.darila@gmail.com>
2017-10-11 16:04:26 +0300

    refactor(entry): iterators

--- main/modules/entry/entry-profile-iterator.ts
+++ main/modules/entry/entry-profile-iterator.ts
@@ -1,11 +1,32 @@
 import * as Promise from 'bluebird';
+import schema from '../utils/jsonschema';
+import { fetchFromPublish } from './helpers';
+import { profileAddress } from '../profile/helpers';
 
+const entryProfileIterator = {
+    'id': '/entryProfileIterator',
+    'type': 'object',
+    'properties': {
+        'limit': { 'type': 'number' },
+        'toBlock': { 'type': 'number' },
+        'akashaId': { 'type': 'string' },
+        'ethAddress': { 'type': 'string', 'format': 'address' }
+    },
+    'required': ['toBlock']
+};
 /**
  * Get entries indexed by publisher
  * @type {Function}
  */
-const execute = Promise.coroutine(function* () {
-    throw new Error('entry:entryProfileIterator deprecated');
+const execute = Promise.coroutine(function* (data: { toBlock: number, limit?: number,
+    lastIndex?: number, ethAddress?: string, akashaId?: string }) {
+
+    const v = new schema.Validator();
+    v.validate(data, entryProfileIterator, { throwError: true });
+
+    const address = yield profileAddress(data);
+    const maxResults = data.limit || 5;
+    return fetchFromPublish(Object.assign({}, data, { limit: maxResults, args: { author: address } }));
 });
 
 export default { execute, name: 'entryProfileIterator' };
--- main/modules/entry/entry-tag-iterator.ts
+++ main/modules/entry/entry-tag-iterator.ts
@@ -1,8 +1,6 @@
 import * as Promise from 'bluebird';
-import contracts from '../../contracts/index';
 import schema from '../utils/jsonschema';
-import resolve from '../registry/resolve-ethaddress';
-import { GethConnector } from '@akashaproject/geth-connector';
+import { fetchFromTagIndex } from './helpers';
 
 const entryTagIterator = {
     'id': '/entryTagIterator',
@@ -10,7 +8,6 @@ const entryTagIterator = {
     'properties': {
         'limit': { 'type': 'number' },
         'toBlock': { 'type': 'number' },
-        'ethAddress': { 'type': 'string', 'format': 'address' },
         'tagName': { 'type': 'string' }
     },
     'required': ['toBlock', 'tagName']
@@ -21,42 +18,12 @@ const entryTagIterator = {
  * @type {Function}
  */
 const execute = Promise.coroutine(function* (data: { toBlock: number, limit?: number,
-    tagName: string, ethAddress?: string, lastIndex?: number }) {
+    tagName: string, lastIndex?: number }) {
     const v = new schema.Validator();
     v.validate(data, entryTagIterator, { throwError: true });
 
-    const collection = [];
     const maxResults = data.limit || 5;
-    const fetched = yield contracts.fromEvent(contracts.instance.Entries.TagIndex,
-        { tagName: data.tagName, author: data.ethAddress }, data.toBlock,
-        maxResults, { lastIndex: data.lastIndex });
-
-    for (let event of fetched.results) {
-        const fetchedPublish = yield contracts
-            .fromEvent(contracts.instance.Entries.Publish, { entryId: event.args.entryId },
-                data.toBlock, 1, {});
-
-        const captureIndex = yield contracts
-            .fromEvent(contracts.instance.Entries.TagIndex, { entryId: event.args.entryId },
-                data.toBlock, 10, {});
-
-        const tags = captureIndex.results.map(function (ev) {
-            return GethConnector.getInstance().web3.toUtf8(ev.args.tagName);
-        });
-
-        const author = yield resolve.execute({ ethAddress: fetchedPublish.results[0].args.author });
-
-        collection.push({
-            entryType: GethConnector.getInstance().web3.toDecimal(event.args.entryType),
-            entryId: event.args.entryId,
-            tags,
-            author
-        });
-        if (collection.length === maxResults) {
-            break;
-        }
-    }
-    return { collection: collection, lastBlock: fetched.fromBlock, lastIndex: fetched.lastIndex };
+    return fetchFromTagIndex(Object.assign({}, data, { limit: maxResults, args: { tagName: data.tagName } }));
 });
 
 export default { execute, name: 'entryTagIterator' };
