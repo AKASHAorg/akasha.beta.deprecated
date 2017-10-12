d566bd0ee066d0520193c8d02e3ff987e85d5aea
Marius Darila <marius.darila@gmail.com>
2017-10-12 14:20:08 +0300

    refactor(getComment): return score also

--- main/modules/comments/get-comment.ts
+++ main/modules/comments/get-comment.ts
@@ -9,23 +9,28 @@ import { unpad } from 'ethereumjs-util';
  * Get comment data for an entry
  * @type {Function}
  */
-const execute = Promise.coroutine(function* (data: { entryId: string, commentId: string }) {
+const execute = Promise.coroutine(function* (data: { entryId: string, commentId: string, noResolve?: boolean }) {
     const [
         parent, ethAddress, deleted,
         publishDate, fn, digestSize, hash
     ] = yield contracts.instance.Comments.getComment(data.entryId, data.commentId);
     const ipfsHash = encodeHash(fn, digestSize, hash);
     const author = yield resolve.execute({ ethAddress: ethAddress });
-    const content = yield getCommentContent(ipfsHash);
+    const content = data.noResolve ? { ipfsHash } : yield getCommentContent(ipfsHash);
+    const [_totalVotes, _score, _endPeriod, , ] = yield contracts.instance.Votes.getRecord(data.commentId);
+
     return Object.assign(
         {},
         content,
         {
-        parent: (!!unpad(parent)) ? parent : null,
-        author,
-        deleted,
-        publishDate: publishDate.toNumber()
-    });
+            parent: (!!unpad(parent)) ? parent : null,
+            author,
+            deleted,
+            totalVotes: _totalVotes.toNumber(),
+            score: _score.toNumber(),
+            endPeriod: _endPeriod.toNumber(),
+            publishDate: publishDate.toNumber()
+        });
 });
 
 export default { execute, name: 'getComment' };
