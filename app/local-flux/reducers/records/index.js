bfabb43c5b7c29ae3547f592bc2d23035987a5d3
Bogdan Brezuica <bogdan.brezuica@akasha.org>
2017-10-13 09:59:28 +0300

    refactor(comments): comment replies

--- app/local-flux/reducers/records/index.js
+++ app/local-flux/reducers/records/index.js
@@ -1,6 +1,6 @@
 export { ActionRecord, ActionState } from './action-record';
 export { AppRecord, NotificationRecord } from './app-record';
-export { CommentData, CommentRecord, CommentsState } from './comments-record';
+export { CommentAuthor, CommentData, CommentRecord, CommentsState } from './comments-record';
 export { ColumnRecord, DashboardRecord, DashboardState, NewColumnRecord } from './dashboard-record';
 export { DraftContent, Draft, CardInfo } from './draft-record';
 export { EntryAuthor, EntryContent, EntryEth, EntryPageOverlay, EntryRecord,
