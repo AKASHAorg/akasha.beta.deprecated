d0757234427771ee456d6d853e6c414d05a9e024
Marius Darila <marius.darila@gmail.com>
2017-10-13 18:05:59 +0300

    feat(comments): add voting

---
+++ main/modules/comments/upvote-comment.ts
@@ -0,0 +1,41 @@
+/*!
+ * Copyright akasha
+ */
+import * as Promise from 'bluebird';
+import contracts from '../../contracts/index';
+import schema from '../utils/jsonschema';
+
+export const upvote = {
+    'id': '/upvote',
+    'type': 'object',
+    'properties': {
+        'entryId': { 'type': 'string' },
+        'token': { 'type': 'string' },
+        'commentId': { 'type': 'string' },
+        'weight': { 'type': 'number' }
+    },
+    'required': ['entryId', 'token', 'commentId', 'weight']
+};
+
+/**
+ * Upvote comment
+ * @type {Function}
+ */
+const execute = Promise.coroutine(function* (data: {
+    entryId: string, weight: number,
+    commentId: string, token: string
+}, cb) {
+
+    const v = new schema.Validator();
+    v.validate(data, upvote, { throwError: true });
+
+    if (data.weight < 1 || data.weight > 10) {
+        throw new Error('Vote weight value must be between 1-10');
+    }
+
+    const txData = contracts.instance.Votes.voteComment.request(data.weight, data.entryId, data.commentId, false, { gas: 200000 });
+    const transaction = yield contracts.send(txData, data.token, cb);
+    return { tx: transaction.tx, receipt: transaction.receipt };
+});
+
+export default { execute, name: 'upvote', hasStream: true };
