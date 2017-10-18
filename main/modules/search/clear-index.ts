3870064659b68c47fa91480f4bc2392d8026e9ac
Marius Darila <marius.darila@gmail.com>
2017-10-18 16:51:34 +0300

    refactor(resolver): throw when 0x0

--- main/modules/search/clear-index.ts
+++ main/modules/search/clear-index.ts
@@ -1,9 +1,30 @@
 import * as Promise from 'bluebird';
 import { dbs } from './indexes';
+import schema from '../utils/jsonschema';
 
-const execute = Promise.coroutine(function* (data, cb) {
-    dbs.entry.searchIndex.flush(function(err) {
-        if (err) { return cb(err); }
+
+const flush = {
+    'id': '/flush',
+    'type': 'object',
+    'properties': {
+        'target': { 'type': 'string' }
+    },
+    'required': ['target']
+};
+
+const execute = Promise.coroutine(function* (data: { target: string }, cb) {
+    const v = new schema.Validator();
+    v.validate(data, flush, { throwError: true });
+
+    const modules = ['entry', 'tags'];
+    if (modules.indexOf(data.target) === -1) {
+        throw new Error('target is not recognized');
+    }
+
+    dbs[data.target].searchIndex.flush(function (err) {
+        if (err) {
+            return cb(err);
+        }
         cb('', { done: true });
     });
     return { done: false };
