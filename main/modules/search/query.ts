c262aac52f925c5bf94d6369b185f4555eb3b8e2
Marius Darila <marius.darila@gmail.com>
2017-10-19 14:10:37 +0300

    refactor(query): add combined search /authors

--- main/modules/search/query.ts
+++ main/modules/search/query.ts
@@ -1,12 +1,42 @@
 import * as Promise from 'bluebird';
 import { dbs } from './indexes';
+import schema from '../utils/jsonschema';
 
-const execute = Promise.coroutine(function* (data: { text: string, pageSize: number, offset: number }, cb) {
+const querySchema = {
+    'id': '/query',
+    'type': 'object',
+    'properties': {
+        'text': { 'type': 'string' },
+        'authors': {
+            'type': 'array',
+            'items': { 'type': 'string' },
+            'uniqueItems': true,
+            'minItems': 1
+        },
+        'pageSize': { 'type': 'number' },
+        'offset': { 'type': 'number' }
+    },
+    'required': ['text']
+};
+
+const buildFilter = function (authors: string[], text: string) {
+    const result = [];
+    authors.forEach((ethAddress) => {
+        result.push({ AND: { 'ethAddress': [ethAddress], 'title': [text] }, BOOST: 5 });
+        result.push({ AND: { 'ethAddress': [ethAddress], 'excerpt': [text] } });
+    });
+    return result;
+};
+
+const execute = Promise.coroutine(function* (data: { text: string, authors?: string[], pageSize: number, offset: number }, cb) {
+    const v = new schema.Validator();
+    v.validate(data, querySchema, { throwError: true });
+
     const collection = [];
     const pageSize = data.pageSize || 20;
     const offset = data.offset || 0;
-    const query = [{ AND: { 'title': [data.text] }, BOOST: 5 }, { AND: { 'excerpt': [data.text] } }];
-
+    const defaultQuery = [{ AND: { 'title': [data.text] }, BOOST: 5 }, { AND: { 'excerpt': [data.text] } }];
+    const query = (data.authors && data.authors.length) ? buildFilter(data.authors, data.text) : defaultQuery;
     dbs.entry.searchIndex.totalHits({ query: query }, function (err, count) {
         dbs.entry.searchIndex.search({
             query: query,
@@ -14,7 +44,11 @@ const execute = Promise.coroutine(function* (data: { text: string, pageSize: num
             offset: offset
         })
             .on('data', (data) => {
-                collection.push({ entryId: data.document.id, ethAddress: data.document.ethAddress, version: data.document.version });
+                collection.push({
+                    entryId: data.document.id,
+                    ethAddress: data.document.ethAddress,
+                    version: data.document.version
+                });
             }).on('end', () => {
             cb('', { collection, totalHits: count, searching: false });
         });
