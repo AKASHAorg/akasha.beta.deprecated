0d49344937cd35ee5d1f34e67d68a449437d3540
Marius Darila <marius.darila@gmail.com>
2017-10-23 17:31:21 +0300

    refactor(chat): add peerCount && getRooms

---
+++ main/modules/chat/peers.ts
@@ -0,0 +1,17 @@
+import * as Promise from 'bluebird';
+import { roomFactory } from './join';
+import { IpfsConnector } from '@akashaproject/ipfs-connector';
+
+const execute = Promise.coroutine(function* (data: { roomName: string }) {
+    const room = roomFactory.getChanPrefix() + data.roomName;
+    return Promise.fromCallback( cb =>
+        IpfsConnector.getInstance().api.apiClient.pubsub.peers(room, (err, resp: string[]) => {
+            if (err) {
+               return cb(err);
+            }
+            cb('', { count: resp.length });
+        })
+    );
+});
+
+export default { execute, name: 'peerCount' };
