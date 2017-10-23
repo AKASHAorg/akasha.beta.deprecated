bbbac47480b8e54ae44615e30d1ca1ad42d57d80
Marius Darila <marius.darila@gmail.com>
2017-10-23 13:08:13 +0300

    refactor(akashaId): use uts46 for normalization

--- main/modules/registry/profile-exists.ts
+++ main/modules/registry/profile-exists.ts
@@ -3,6 +3,7 @@ import contracts from '../../contracts/index';
 import { unpad } from 'ethereumjs-util';
 import schema from '../utils/jsonschema';
 import { checkIdFormat } from './check-id-format';
+import { normaliseId } from '../profile/helpers';
 
 /**
  * Check if provided `akashaId` is taken
@@ -11,10 +12,12 @@ import { checkIdFormat } from './check-id-format';
 const execute = Promise.coroutine(function* (data: ProfileExistsRequest) {
     const v = new schema.Validator();
     v.validate(data, checkIdFormat, { throwError: true });
-    const idHash = yield contracts.instance.ProfileRegistrar.hash(data.akashaId);
+
+    const normalisedId = normaliseId(data.akashaId);
+    const idHash = yield contracts.instance.ProfileRegistrar.hash(normalisedId);
     const exists = yield contracts.instance.ProfileResolver.addr(idHash);
-    const idValid = yield contracts.instance.ProfileRegistrar.check_format(data.akashaId);
-    return { exists: !!unpad(exists), idValid, akashaId: data.akashaId };
+    const idValid = yield contracts.instance.ProfileRegistrar.check_format(normalisedId);
+    return { exists: !!unpad(exists), idValid, akashaId: data.akashaId, normalisedId };
 });
 
 export default { execute, name: 'profileExists' };
