2f333988b37011aae797e0cb1c30fa199f4b09e6
Marius Darila <marius.darila@gmail.com>
2017-11-22 17:00:12 +0200

    fix(follow): normalize time events

--- main/modules/profile/following-iterator.ts
+++ main/modules/profile/following-iterator.ts
@@ -4,13 +4,16 @@ import contracts from '../../contracts/index';
 import { profileAddress } from './helpers';
 import schema from '../utils/jsonschema';
 import { followersIterator } from './followers-iterator';
+import { uniq } from 'ramda';
 
 /**
  * Get followed profiles of id
  * @type {Function}
  */
-const execute = Promise.coroutine(function* (data: { lastBlock?: number, limit?: number,
-    akashaId?: string, ethAddress?: string, lastIndex?: number }) {
+const execute = Promise.coroutine(function* (data: {
+    lastBlock?: number, limit?: number,
+    akashaId?: string, ethAddress?: string, lastIndex?: number
+}) {
     const v = new schema.Validator();
     v.validate(data, followersIterator, { throwError: true });
 
@@ -18,11 +21,21 @@ const execute = Promise.coroutine(function* (data: { lastBlock?: number, limit?:
     const maxResults = data.limit || 5;
     const address = yield profileAddress(data);
     const toBlock = (!data.lastBlock) ? yield GethConnector.getInstance().web3.eth.getBlockNumberAsync() : data.lastBlock;
-    const fetched = yield contracts.fromEvent(contracts.instance.Feed.Follow, { follower: address }, toBlock, maxResults, {lastIndex: data.lastIndex});
+    const fetched = yield contracts.fromEvent(contracts.instance.Feed.Follow, { follower: address }, toBlock, maxResults, { lastIndex: data.lastIndex });
     for (let event of fetched.results) {
+        const follows = yield contracts.instance.Feed.follows(address, event.args.followed);
+        if (!follows) {
+            continue;
+        }
         collection.push({ ethAddress: event.args.followed });
     }
-    return { collection: collection, lastBlock: fetched.fromBlock, lastIndex: fetched.lastIndex, akashaId: data.akashaId, limit: maxResults };
+    return {
+        collection: uniq(collection),
+        lastBlock: fetched.fromBlock,
+        lastIndex: fetched.lastIndex,
+        akashaId: data.akashaId,
+        limit: maxResults
+    };
 });
 
 export default { execute, name: 'followingIterator' };
