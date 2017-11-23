08a0b4afa4d30c0acd7799ba7a68ff96436d428d
Marius Darila <marius.darila@gmail.com>
2017-11-23 18:56:41 +0200

    feat(profile): add votes iterator

--- main/channels.ts
+++ main/channels.ts
@@ -32,7 +32,7 @@ const channels = {
         'unFollowProfile', 'updateProfileData', 'followersIterator', 'followingIterator', 'isFollower', 'isFollowing',
         'getFollowingList', 'getProfileList', 'tip', 'resolveProfileIpfsHash', 'toggleDonations', 'bondAeth',
         'cycleAeth', 'freeAeth', 'transformEssence', 'manaBurned', 'cyclingStates', 'transfer', 'transfersIterator',
-        'essenceIterator', 'commentsIterator', 'getCommentsCount'],
+        'essenceIterator', 'commentsIterator', 'getCommentsCount', 'votesIterator'],
 
     registry: ['fetchRegistered', 'addressOf', 'checkIdFormat', 'getCurrentProfile', 'profileExists', 'registerProfile',
         'getByAddress', 'unregister'],
