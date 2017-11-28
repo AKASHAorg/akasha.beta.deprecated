e33e58838d66a0ee853391e9bd9bc40ea9147178
Sever Abibula (SeverS) <sever.abibula@akasha.org>
2017-11-28 17:47:59 +0200

    refactor(entry page): entry page images now have a proper class

---
+++ app/components/svg/new-icons/close-entry-option.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const CloseEntryOption = props => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" {...props}>
+    <g fill="none" fillRule="evenodd" transform="translate(1.5 1.5)">
+      <circle cx="10.5" cy="10.5" r="10.5" strokeWidth="1.5" />
+      <g fill="#2E3747" transform="rotate(45 1.866 13.42)">
+        <rect width="1.313" height="10.5" x="5.25" rx=".656" />
+        <rect width="1.313" height="10.5" x="5.156" y=".094" transform="rotate(90 5.906 5.344)" rx=".656" />
+      </g>
+    </g>
+  </svg>
+);
+
+export default CloseEntryOption;
