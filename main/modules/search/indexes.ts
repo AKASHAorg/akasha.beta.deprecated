dc7a232a25c7022c528bc42a1c8da335ba05f738
Marius Darila <marius.darila@gmail.com>
2017-12-14 18:26:10 +0200

    refactor(indexes): add nGram composite search

--- main/modules/search/indexes.ts
+++ main/modules/search/indexes.ts
@@ -5,21 +5,35 @@ import { join as pathJoin } from 'path';
 export const dbs = {
     entry: {
         path: pathJoin(app.getPath('userData'), 'entry-index'),
+        additional: {
+            fieldOptions: {
+                excerpt: {
+                    searchable: true,
+                    preserveCase: false
+                },
+                title: {
+                    searchable: true,
+                    preserveCase: false
+                }
+            }
+        },
         searchIndex: null
     },
     tags: {
         path: pathJoin(app.getPath('userData'), 'tags-index'),
-        searchIndex: null
+        searchIndex: null,
+        additional: {}
     },
     profiles: {
         path: pathJoin(app.getPath('userData'), 'profileID-index'),
-        searchIndex: null
+        searchIndex: null,
+        additional: {}
     }
 };
 
 export default function init() {
     const waitFor = Object.keys(dbs).map((index) => {
-        return new Storage(dbs[index].path).init().then(si => dbs[index].searchIndex = si);
+        return new Storage(dbs[index].path, dbs[index].additional).init().then(si => dbs[index].searchIndex = si);
     });
     return Promise.all(waitFor);
 }
