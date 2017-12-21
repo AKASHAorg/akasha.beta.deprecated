586180d075e1287a2d217b71da98e5059bf0515f
Vali Cotea <quinine35@gmail.com>
2017-12-21 16:05:56 +0200

    chore(): add tooltips to sidebar icons

--- app/components/sidebar-icon.js
+++ app/components/sidebar-icon.js
@@ -3,10 +3,11 @@ import React from 'react';
 import { withRouter } from 'react-router';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames';
+import { Tooltip } from 'antd';
 import { Icon } from './';
 
 const SidebarIcon = (props) => {
-    const { activePath, className, iconType, linkTo, location } = props;
+    const { activePath, className, iconType, linkTo, location, tooltipTitle, disabled } = props;
     const isActive = location.pathname.includes(activePath);
     const wrapperClassName = classNames('flex-center sidebar-icon__wrapper', {
         'sidebar-icon__wrapper_active': isActive,
@@ -14,12 +15,29 @@ const SidebarIcon = (props) => {
     const iconClassName = classNames('sidebar-icon__icon', className, {
         'sidebar-icon__icon_active': isActive
     });
+    if (disabled) {
+        return (
+          <Tooltip
+            title={tooltipTitle}
+            placement="topLeft"
+          >
+            <div className={wrapperClassName}>
+              <Icon className={iconClassName} type={iconType} />
+            </div>
+          </Tooltip>
+        );
+    }
 
     return (
       <Link to={linkTo || ''}>
-        <div className={wrapperClassName}>
-          <Icon className={iconClassName} type={iconType} />
-        </div>
+        <Tooltip
+          title={tooltipTitle}
+          placement="topLeft"
+        >
+          <div className={wrapperClassName}>
+            <Icon className={iconClassName} type={iconType} />
+          </div>
+        </Tooltip>
       </Link>
     );
 };
@@ -27,9 +45,11 @@ const SidebarIcon = (props) => {
 SidebarIcon.propTypes = {
     activePath: PropTypes.string.isRequired,
     className: PropTypes.string,
+    disabled: PropTypes.bool,
     iconType: PropTypes.string.isRequired,
     linkTo: PropTypes.string,
     location: PropTypes.shape().isRequired,
+    tooltipTitle: PropTypes.string
 };
 
 export default withRouter(SidebarIcon);
