1e90046733557cf0a1318ce895ffe2a11ec93b0a
Vali Cotea <quinine35@gmail.com>
2018-01-04 18:18:58 +0200

    chore(): prevent logout by navigation

--- app/index.js
+++ app/index.js
@@ -18,7 +18,19 @@ if (process.env.DARK_THEME) {
     require('./styles/ant-vars/extract-default-theme.less');
 }
 
-const history = createHashHistory();
+const history = createHashHistory({
+    getUserConfirmation (message, callback) {
+        return callback(false);
+    }
+});
+
+history.block((location, action) => {
+    if ((location.pathname === '/setup/authenticate' || location.pathname === '/setup/new-identity-interests')
+        && action === 'POP') {
+        return '';
+    }
+    return null;
+});
 const store = configureStore();
 sagaMiddleware.run(rootSaga);
 
