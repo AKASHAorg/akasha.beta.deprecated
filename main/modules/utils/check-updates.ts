265a07fbfcf249269b87eeac909d6eb3b73c1b3b
Marius Darila <marius.darila@gmail.com>
2018-01-18 15:18:49 +0200

    feat(autoupdater): add hazel integration

--- main/modules/utils/check-updates.ts
+++ main/modules/utils/check-updates.ts
@@ -1,17 +1,7 @@
-import contracts from '../../contracts/index';
-import { GethConnector } from '@akashaproject/geth-connector';
-import updater from '../../check-version';
 import * as Promise from 'bluebird';
 
 const execute = Promise.coroutine(function* () {
-    return contracts
-        .instance
-        .feed
-        .contract
-        .getAppState((err, state) => {
-            const version = GethConnector.getInstance().web3.toUtf8(state[0]);
-            return updater.checkVersion(version, state[1], state[2]);
-        });
+
 });
 
 export default { execute, name: 'checkUpdate' };
