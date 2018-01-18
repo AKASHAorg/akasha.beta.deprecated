ea20ef319d44e9259ca3c9545d07f1d0a0f43602
Marius Darila <marius.darila@gmail.com>
2018-01-18 19:39:25 +0200

    refactor(autoupdater): add channel

--- main/modules/utils/check-updates.ts
+++ main/modules/utils/check-updates.ts
@@ -1,7 +1,14 @@
 import * as Promise from 'bluebird';
+import { autoUpdater } from 'electron';
 
 const execute = Promise.coroutine(function* () {
-
+    if (
+        process.env.NODE_ENV !== 'development' &&
+        (process.platform === 'darwin' || process.platform === 'win32')
+    ) {
+        autoUpdater.checkForUpdates();
+    }
+    return Promise.resolve({ done: true });
 });
 
 export default { execute, name: 'checkUpdate' };
