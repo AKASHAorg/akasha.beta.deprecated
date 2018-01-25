3fb546327876a048eea2813ee7d01d553b49a448
Marius Darila <marius.darila@gmail.com>
2018-01-25 18:37:53 +0200

    refactor(voteEndPeriod): accept array of entries

--- main/modules/entry/vote-endperiod.ts
+++ main/modules/entry/vote-endperiod.ts
@@ -4,19 +4,20 @@ import schema from '../utils/jsonschema';
 
 const voteEndPeriod = {
     'id': '/voteEndPeriod',
-    'type': 'object',
-    'properties': {
-        'entryId': { 'type': 'string' }
-    },
-    'required': ['entryId']
+    'type': 'array',
+    'items': { 'type': 'string' },
+    'minItems': 1
 };
 
-const execute = Promise.coroutine(function* (data: { entryId: string }) {
+const execute = Promise.coroutine(function* (data: string[]) {
     const v = new schema.Validator();
     v.validate(data, voteEndPeriod, { throwError: true });
-
-    const record = yield contracts.instance.Votes.getRecord(data.entryId);
-    return { endDate: (record[2]).toNumber() };
+    const collection = [];
+    for (let i = 0; i < data.length; i++) {
+        const record = yield contracts.instance.Votes.getRecord(data[i]);
+        collection.push({entryId: data[i], endDate: (record[2]).toNumber()});
+    }
+    return { collection };
 });
 
 export default { execute, name: 'getVoteEndPeriod' };
