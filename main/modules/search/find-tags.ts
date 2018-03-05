121d5a38d241f9c33a73311adef34b9f30500359
Marius Darila <marius.darila@gmail.com>
2018-03-05 17:29:00 +0200

    refactor(tagIndex): hash based search

--- main/modules/search/find-tags.ts
+++ main/modules/search/find-tags.ts
@@ -1,19 +1,22 @@
 import * as Promise from 'bluebird';
 import { dbs } from './indexes';
+import GethConnector from '@akashaproject/geth-connector/lib/GethConnector';
 
 const execute = Promise.coroutine(function* (data: { text: string, limit: number }, cb) {
-    const collection = [];
+    const collection = new Set();
     const pageSize = data.limit || 10;
     const options = {
-        beginsWith: data.text,
+        beginsWith: GethConnector.getInstance().web3.fromUtf8(data.text),
         field: 'tagName',
         threshold: 1,
         limit: pageSize,
         type: 'simple'
     };
     dbs.tags.searchIndex.match(options)
-        .on('data', (data) => { collection.push(data); })
-        .on('end', () => { cb('', { collection});
+        .on('data', (data) => {
+            collection.add(GethConnector.getInstance().web3.toUtf8(data));
+        })
+        .on('end', () => { cb('', { collection: Array.from(collection)});
     });
     return {};
 });
