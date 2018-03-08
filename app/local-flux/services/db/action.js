f6ebfe2d349c5dc32cbcd2944b8c7fdacecaecd1
Marius Darila <marius.darila@gmail.com>
2018-03-08 19:39:14 +0200

    refactor(dashboardDb): schema && service

--- app/local-flux/services/db/action.js
+++ app/local-flux/services/db/action.js
@@ -1,10 +1,7 @@
-import { akashaDB } from './dbs';
-
-export const collectionName = `action-akasha-${process.env.AKASHA_VERSION}-${process.env.NODE_ENV}`;
-export const options = {
-    indices: ['ethAddress', 'status', 'type', 'id', 'created', 'claimed'],
-    unique: ['tx', 'id']
+export default {
+    collectionName : `action-akasha-${process.env.AKASHA_VERSION}-${process.env.NODE_ENV}`,
+    options : {
+        indices: ['ethAddress', 'status', 'type', 'id', 'created', 'claimed'],
+        unique: ['tx', 'id']
+    }
 };
-const actionCollection = () => akashaDB.getCollection(collectionName);
-
-export default actionCollection;
