e02cc8c2a7397fd59007f1336fec9f08539e71ec
Marius Darila <marius.darila@gmail.com>
2018-03-12 15:45:46 +0200

    refactor(db): tmp save

--- app/local-flux/services/db/dbs.js
+++ app/local-flux/services/db/dbs.js
@@ -2,25 +2,49 @@ import Loki from 'lokijs';
 import LokiIndexedAdapter from 'lokijs/src/loki-indexed-adapter';
 import actionCollection from './action';
 import dashboardCollection from './dashboard';
+import entriesCollection from './entry';
+import highlightCollection from './highlight';
+import listCollection from './list';
+import profileCollection from './profile';
+import searchCollection from './search';
+import settingsCollection from './settings';
 
 const idbAdapter = new LokiIndexedAdapter('aka-shard');
 const pa = new Loki.LokiPartitioningAdapter(idbAdapter, {paging: true});
-const collections = [actionCollection, dashboardCollection];
+const collections = [
+    actionCollection,
+    dashboardCollection,
+    entriesCollection,
+    highlightCollection,
+    listCollection,
+    profileCollection,
+    searchCollection,
+    settingsCollection
+];
 
 export const akashaDB = new Loki('akashaDB-beta', {
     adapter: pa,
-    autoload: true,
-    autoloadCallback: function () {
-        collections.forEach(record => {
-            console.log('actions', record.collectionName);
-            if (!akashaDB.getCollection(record.collectionName)) {
-                akashaDB.addCollection(record.collectionName, record.options);
-            }
-        });
-    },
+    autoload: false,
     autosave: true,
     autosaveInterval: 4000
 });
 
+export const loadAkashaDB  = () => new Promise(resolve =>
+    akashaDB.loadDatabase({}, () => {
+        collections.forEach(record => {
+            if (!akashaDB.getCollection(record.collectionName)) {
+                akashaDB.addCollection(record.collectionName, record.options);
+            }
+        });
+        resolve();
+    })
+);
+
 export const getActionCollection = () => akashaDB.getCollection(actionCollection.collectionName);
 export const getDashboardCollection = () => akashaDB.getCollection(dashboardCollection.collectionName);
+export const getEntriesCollection = () => akashaDB.getCollection(entriesCollection.collectionName);
+export const getHighlightCollection = () => akashaDB.getCollection(highlightCollection.collectionName);
+export const getListCollection = () => akashaDB.getCollection(listCollection.collectionName);
+export const getProfileCollection = () => akashaDB.getCollection(profileCollection.collectionName);
+export const getSearchCollection = () => akashaDB.getCollection(searchCollection.collectionName);
+export const getSettingsCollection = () => akashaDB.getCollection(settingsCollection.collectionName);
