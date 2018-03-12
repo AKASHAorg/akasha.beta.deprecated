2f68d6b83bcf3a0da1605c066c8cc54bed35b2f5
Marius Darila <marius.darila@gmail.com>
2018-03-12 20:40:41 +0200

    refactor(db): migrate to lokijs

--- app/local-flux/services/search-service.js
+++ app/local-flux/services/search-service.js
@@ -1,33 +1,57 @@
-import searchDB from './db/search';
+import * as Promise from 'bluebird';
+import {getSearchCollection} from './db/dbs';
 
-export const getLastEntriesBlock = ethAddress =>
-    searchDB.lastEntriesBlock
-        .where('ethAddress')
-        .equals(ethAddress)
-        .first()
-        .then((data) => {
-            if (!data) {
-                return 0;
-            }
-            return data.blockNr;
-        });
+const LAST_ENTRY_TYPE = 'lastEntry';
+const LAST_TAG_TYPE = 'lastTag';
 
-export const getLastTagsBlock = type =>
-    searchDB.lastTagsBlock
-        .where('type')
-        .equals(type)
-        .first()
-        .then((data) => {
-            if (!data) {
-                return null;
-            }
-            return data.blockNr;
-        });
+export const getLastEntriesBlock = ethAddress => {
+    try {
+        const record = getSearchCollection().findOne({id: ethAddress, opType: LAST_ENTRY_TYPE});
+        return Promise.resolve(record.blockNr);
+    }catch (error) {
+        return Promise.reject(error);
+    }
+};
 
-export const updateLastEntriesBlock = ({ ethAddress, blockNr }) =>
-    searchDB.lastEntriesBlock
-        .put({ ethAddress, blockNr });
+export const getLastTagsBlock = type => {
+    try {
+        const record = getSearchCollection().findOne({opType: LAST_TAG_TYPE, id: type});
+        return Promise.resolve(record.blockNr);
+    }catch (error) {
+        return Promise.reject(error);
+    }
+};
 
-export const updateLastTagsBlock = ({ type, blockNr }) =>
-    searchDB.lastTagsBlock
-        .put({ type, blockNr });
+export const updateLastEntriesBlock = ({ ethAddress, blockNr }) => {
+    try {
+        const record = getSearchCollection().findOne({id: ethAddress, opType: LAST_ENTRY_TYPE});
+        if(record) {
+            getSearchCollection()
+                .findAndUpdate(
+                    {id: ethAddress, opType: LAST_ENTRY_TYPE},
+                    rec => rec.blockNr = blockNr);
+        } else {
+            getSearchCollection().insert({id: ethAddress, opType: LAST_ENTRY_TYPE, blockNr: blockNr})
+        }
+        return Promise.resolve(true);
+    }catch (error) {
+        return Promise.reject(error);
+    }
+};
+
+export const updateLastTagsBlock = ({ type, blockNr }) => {
+    try {
+        const record = getSearchCollection().findOne({id: type, opType: LAST_TAG_TYPE});
+        if(record) {
+            getSearchCollection()
+                .findAndUpdate(
+                    {id: type, opType: LAST_TAG_TYPE},
+                    rec => rec.blockNr = blockNr);
+        } else {
+            getSearchCollection().insert({id: type, opType: LAST_TAG_TYPE, blockNr: blockNr})
+        }
+        return Promise.resolve(true);
+    }catch (error) {
+        return Promise.reject(error);
+    }
+};
