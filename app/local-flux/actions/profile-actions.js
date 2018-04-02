81762f636ffe7a87622db1c11f4bb08c6a64035a
Vali Cotea <quinine35@gmail.com>
2018-04-02 11:23:09 +0300

    chore(): add profile follow and unfollow errors

--- app/local-flux/actions/profile-actions.js
+++ app/local-flux/actions/profile-actions.js
@@ -90,6 +90,7 @@ export const profileFollow = ({ actionId, ethAddress }) =>
 export const profileFollowError = (error, request) => {
     error.code = 'PFE01';
     error.messageId = 'profileFollow';
+    error.values = { ethAddress: request.ethAddress };
     return action(types.PROFILE_FOLLOW_ERROR, { error, request });
 };
 
@@ -391,6 +392,7 @@ export const profileUnfollow = ({ actionId, ethAddress }) =>
 export const profileUnfollowError = (error, request) => {
     error.code = 'PUE01';
     error.messageId = 'profileUnfollow';
+    error.values = { ethAddress: request.ethAddress };
     return action(types.PROFILE_UNFOLLOW_ERROR, { error, request });
 };
 
