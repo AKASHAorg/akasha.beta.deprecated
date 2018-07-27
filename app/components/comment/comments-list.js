dc2700750417ea623ccf526a223d77df5ac68499
Sever Abibula <sever.abi@gmail.com>
2018-07-27 14:30:16 +0300

    fix():

--- app/components/comment/comments-list.js
+++ app/components/comment/comments-list.js
@@ -91,7 +91,7 @@ class CommentList extends Component {
                 loggedProfileData={loggedProfileData}
               />
             ))}
-            {!fetchingComments && comments.map(comm => (
+            {!fetchingComments && comments.filter(c => c.entryId === entryId).map(comm => (
               <CommentThread
                 comment={comm}
                 containerRef={containerRef}
@@ -108,7 +108,7 @@ class CommentList extends Component {
                 <Spin />
               </div>
             }
-            {!fetchingComments && !commentsCount && !optimisticComments.length &&
+            {!fetchingComments && !commentsCount && !optimisticComments.length && !comments.size &&
               <div className="comment-list__placeholder">
                 <div>{intl.formatMessage(entryMessages.noCommentsFound)}</div>
                 <div>
