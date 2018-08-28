10cf266839484dfc7d15194fcd271a951b7dfd16
Sever Abibula <sever.abibula@akasha.org>
2018-08-28 16:42:26 +0300

    Fix/#314 community (#401)
    * fix test env
    
    * chore(): add tag-editor tests
    
    * chore(): add more tests, fix issue /Community/#238, minor css fix for leazy image loader
    
    * chore(featured image): issue #232
    
    * cleanup
    
    * fixes(): various fixes
    
    * fix(login button): add more info when login button is disabled

--- test/app/setup.js
+++ test/app/setup.js
@@ -1,19 +1,19 @@
-// import 'babel-polyfill';
-// import { jsdom } from 'jsdom';
-// import hook from 'css-modules-require-hook';
+import { JSDOM } from 'jsdom';
 import sinon from 'sinon';
 import Channel from './helpers/channels';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import { createActionChannels } from '../../app/local-flux/sagas/helpers';
 
+const dom = new JSDOM('<!doctype html><html><body></body></html>');
+global.window = dom.window;
+global.document = dom.window.document;
+global.navigator = global.window.navigator;
+
 before('init tests', function () {
-    this.sandbox = sinon.sandbox.create();
+    this.sandbox = sinon.createSandbox();
     createActionChannels(Channel);
+    Enzyme.configure({
+        adapter: new Adapter()
+    });
 });
-
-// hook({
-//     generateScopedName: '[name]__[local]___[hash:base64:5]'
-// });
-
-// global.document = jsdom('<!doctype html><html><body></body></html>');
-// global.window = global.document.defaultView;
-// global.navigator = global.window.navigator;
