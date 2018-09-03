5e90b5c88a008b2ba208acbd6a7784e04a1aaa19
Sever Abibula <sever.abi@gmail.com>
2018-09-03 10:22:26 +0300

    chore(): add missing translation strings

--- app/components/panels/notifications-panel.js
+++ app/components/panels/notifications-panel.js
@@ -136,54 +136,13 @@ class NotificationsPanel extends Component {
     }
 
     renderNotifications = () => {
-        const { darkTheme, intl, notifications, notificationsLoaded } = this.props;
-        const imgClass = classNames('notifications-panel__placeholder', {
-            'notifications-panel__placeholder_dark': darkTheme
-        });
-
-        if (!notificationsLoaded) {
-            return (
-              <div className="notifications-panel__timeline-wrapper">
-                <DataLoader flag />
-              </div>
-            );
-        }
-
-        return (
-          <div className="notifications-panel__timeline-wrapper" ref={this.getContainerRef}>
-            <Timeline>
-              {notifications.map((notif) => {
-                  if (!notif.blockNumber) {
-                      return null;
-                  }
-                  return (
-                    <Item
-                      color="red"
-                      key={this.getUniqueKey(notif)}
-                    >
-                      <NotificationLog
-                        containerRef={this.containerRef}
-                        key={notif.blockNumber}
-                        notification={notif}
-                      />
-                    </Item>
-                  );
-              })}
-            </Timeline>
-            {!notifications.size &&
-              <div className="flex-center notifications-panel__placeholder-wrapper">
-                <div className={imgClass} />
-                <div className="notifications-panel__placeholder-message">
-                  {intl.formatMessage(profileMessages.noNotifications)}
-                </div>
-              </div>
-            }
-          </div>
-        );
+        
     };
 
     render () {
-        const { intl } = this.props;
+        const { intl, userSettings } = this.props;
+        const { feed, comments, donations, votes } = userSettings.get('notificationsPreference');
+        console.log(feed, comments, donations, votes);
 
         return (
           <div className="notifications-panel">
