59262f02225649a166272081ce58b5ac20b6c5cc
Marius Darila <marius.darila@gmail.com>
2019-04-05 11:44:25 +0200

    chore: cleanup

--- channels/profile/current-balance.ts
+++ channels/profile/current-balance.ts
@@ -22,7 +22,7 @@ export default function init(sp, getService) {
       data.ethAddress : web3Api.instance.eth.defaultAccount;
 
     const unit = (data.unit) ? data.unit : 'ether';
-    const utils.fromWei = web3Api.instance.utils.fromWei;
+    const fromWei = web3Api.instance.utils.fromWei;
     const weiAmount = yield web3Api.instance.eth.getBalance(etherBase);
     const [free, bonded, cycling] = yield contracts
     .instance.AETH.getTokenRecords(etherBase);
@@ -36,24 +36,24 @@ export default function init(sp, getService) {
     const essenceValue = yield contracts.instance.Essence.aethValueFrom(essence);
     const symbol = 'AETH'; // yield contracts.instance.AETH.symbol();
     const totalAeth = free.plus(bonded).plus(cycling);
-    const balance = utils.fromWei(weiAmount, unit);
+    const balance = fromWei(weiAmount, unit);
     return {
       balance: balance.toFormat(5),
       [symbol]: {
-        total: (utils.fromWei(totalAeth)).toFormat(7),
-        free: (utils.fromWei(free)).toFormat(5),
-        bonded: (utils.fromWei(bonded)).toFormat(5),
-        cycling: (utils.fromWei(cycling)).toFormat(5),
+        total: (fromWei(totalAeth)).toFormat(7),
+        free: (fromWei(free)).toFormat(5),
+        bonded: (fromWei(bonded)).toFormat(5),
+        cycling: (fromWei(cycling)).toFormat(5),
       },
       mana: {
-        total: (utils.fromWei(manaTotal)).toFormat(5),
-        spent: (utils.fromWei(manaSpent)).toFormat(5),
-        remaining: (utils.fromWei(manaRemaining)).toFormat(5),
+        total: (fromWei(manaTotal)).toFormat(5),
+        spent: (fromWei(manaSpent)).toFormat(5),
+        remaining: (fromWei(manaRemaining)).toFormat(5),
       },
-      karma: { total: (utils.fromWei(karma)).toFormat(5) },
+      karma: { total: (fromWei(karma)).toFormat(5) },
       essence: {
-        total: (utils.fromWei(essence)).toFormat(5),
-        aethValue: (utils.fromWei(essenceValue)).toFormat(5),
+        total: (fromWei(essence)).toFormat(5),
+        aethValue: (fromWei(essenceValue)).toFormat(5),
       }
       , unit, etherBase,
     };
